205721d09e58066509581e983d70c2d217b482fb
oric99 <70826513+oric99@users.noreply.github.com>
2022-02-16 23:38:06 -0500

    printPrimes complete

--- Practices/02.Clean-Code/PrintPrimes/PrintPrimes/Program.cs
+++ Practices/02.Clean-Code/PrintPrimes/PrintPrimes/Program.cs
@@ -6,12 +6,16 @@ namespace PrintPrimes
   {
     static void Main(string[] args)
     {
+      Console.WriteLine("---------");
       PrintPrimes(13);
       // 2,3,5,7,11,13
+      Console.WriteLine("");
       PrintPrimes(10);
       // 2,3,5,7
+      Console.WriteLine("");
       PrintPrimes(0);
       // Error: Invalid Number
+      Console.WriteLine(" ");
       PrintPrimes(1);
       // Error: 1 Is not Prime
 
@@ -19,26 +23,39 @@ namespace PrintPrimes
 
     private static void PrintPrimes(int number)
     {
-        try
+      int cont = 0;
+      try
+      {
+        if (number==0)
         {
-          for (int i = 0; i <= number; i++)
+          Console.Write("Error: Invalid Number");
+        }else if (number==1)
+        {
+          Console.Write("Error: 1 is not Prime");
+        }else
+        {
+          for (int i = 2; i <= number; i++)
           {
-            for (int j = 1; j < i; j++)
-            {
-              if (i%j==0)
-              {
-                Console.WriteLine(number);
+            for (int j = 1; j <= i; j++)
+            { 
+              if (i % j == 0){ 
+                cont = cont + 1;
               }
             }
-          }
-        }
-        catch (System.Exception)
-        {
-          Console.WriteLine("no funca");
-          throw;
+            if (cont <= 2){ 
+              Console.Write(i+",");
+            }      
+            cont = 0;
+          } 
         }
         
       }
+      catch (System.Exception)
+      {
+        Console.WriteLine("Error de compilación mi broder");
+        throw;
+      }
+      
     }
   }
 }
