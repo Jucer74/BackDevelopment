71bcc340b087059ab868407b47269c0c1a0b9673
JCorn39 <88836018+JCorn39@users.noreply.github.com>
2022-02-16 21:30:02 -0500

    Numeros Primos 1

--- Practices/02.Clean-Code/PrintPrimes/PrintPrimes/Program.cs
+++ Practices/02.Clean-Code/PrintPrimes/PrintPrimes/Program.cs
@@ -8,18 +8,37 @@ namespace PrintPrimes
     {
       PrintPrimes(13);
       // 2,3,5,7,11,13
+      Console.WriteLine ("-13-");
       PrintPrimes(10);
       // 2,3,5,7
+      Console.WriteLine ("-10-");
       PrintPrimes(0);
       // Error: Invalid Number
+      Console.WriteLine ("-0-");
       PrintPrimes(1);
       // Error: 1 Is not Prime
+      Console.WriteLine ("-1-");
 
     }
 
     private static void PrintPrimes(int number)
     {
-      throw new NotImplementedException();
+      int cont = 0;
+        for (int i = 2; i <= number; i++){  
+  for (int j = 1; j <= i; j++){  
+ 
+               if (i % j == 0){ 
+                  cont = cont + 1;
+               }
+            }
+    
+            if (cont <= 2){ 
+               Console.WriteLine(i);
+            }
+            cont = 0;
+    
+         }
+         Console.ReadKey();
     }
   }
 }
