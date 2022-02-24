2ee4bc486c3f21f89809a3a51302c7d4dbe1d1eb
JCorn39 <88836018+JCorn39@users.noreply.github.com>
2022-02-23 21:26:14 -0500

    Multiplos 1

---
+++ Practices/02.Clean-Code/PrintMultiples/PrinMultiples/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PrinMultiples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Del 1 al 100");
+            for (int x=1;x<=100;x++){
+               
+                if (x%3==0){
+                    Console.WriteLine("Numero: "+x+" M-3");
+                }
+                else if(x%5==0){
+                     Console.WriteLine("Numero: "+x+" M-5");
+                }
+                else if(x%3==0 && x%5==0){
+                    Console.WriteLine("Numero: "+x+" M-3-5");
+                }
+                else{
+                     Console.WriteLine("Numero: "+x);
+                }
+            }
+        }
+    }
+}
