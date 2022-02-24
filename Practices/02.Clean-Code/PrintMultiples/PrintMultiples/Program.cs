4a7f8049383dbf66bd0017e2b651086d83b78d17
Luis0725 <luisym0725@gmail.com>
2022-02-23 21:13:25 -0500

    multiplos

---
+++ Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrintMultiples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
