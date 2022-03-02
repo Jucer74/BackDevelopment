b4d333f82ba767a4cb1d5336a3868e8e4618be9e
Luis0725 <luisym0725@gmail.com>
2022-03-01 19:35:22 -0500

    Multiplos

--- Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
+++ Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
 
-namespace PrintMultiples
+namespace PrintverificarMultiplos
 {
     class Program
     {
-        static void Main(string[] args)
+        static  void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+                for(int numero=1; numero<=100; numero++)
+                {
+                
+                    if(verificarMultiplos(numero) == 0)
+                    {
+                     Console.Write("{0}," , numero);
+                    }
+
+                    if(verificarMultiplos(numero)  == 3)
+                    {
+                    Console.Write("M-3,");
+                    }
+
+                    if(verificarMultiplos(numero) == 5)
+                    {
+                    Console.Write("M-5,");
+                    }
+
+                    if(verificarMultiplos(numero) == 1)
+                    {
+                    Console.Write("M-3-5,");
+                    }
+               }
+          }
+           
+     
+           
+        public static int verificarMultiplos(int cifra)
+        {
+            if(cifra % 3 ==0)
+            {  
+                if(cifra % 5 ==0)
+                {
+                    return 1;
+                }
+                return 3;     
+            }
+
+            if(cifra % 5 ==0)
+            {
+                return 5;
+            }
+
+             /* if(cifra % 3 == 0 && cifra % 5 == 0)
+            {
+                return 1;
+            }  */
+
+            return 0;
         }
     }
 }
+
