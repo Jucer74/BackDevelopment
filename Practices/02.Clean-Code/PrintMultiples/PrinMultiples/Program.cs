6fb9103a5de10e3890a20c356d01fd05a21e6f42
JCorn39 <88836018+JCorn39@users.noreply.github.com>
2022-03-02 18:01:21 -0500

    MultiplosClean

--- Practices/02.Clean-Code/PrintMultiples/PrinMultiples/Program.cs
+++ Practices/02.Clean-Code/PrintMultiples/PrinMultiples/Program.cs
@@ -6,21 +6,28 @@ namespace PrinMultiples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Del 1 al 100");
-            for (int x=1;x<=100;x++){
-               
-                if(x%15==0){
-                    Console.WriteLine("Numero: "+x+" M-3-5");
-                }
-                else if(x%5==0){
-                     Console.WriteLine("Numero: "+x+" M-5");
-                }
-                else if(x%3==0){
-                    Console.WriteLine("Numero: "+x+" M-3");
-                }
-                else{
-                     Console.WriteLine("Numero: "+x);
-                }
+            Console.WriteLine("|"+"~~~~~~~~~~~~~~~~~~~~"+"|"+"~~~~~~~~~~~~~~~"+"|");
+            Console.WriteLine("|"+"Numeros Del 1 al 100"+"|"+"Multiplos 3 o 5"+"|");
+            Console.WriteLine("|"+"~~~~~~~~~~~~~~~~~~~~"+"|"+"~~~~~~~~~~~~~~~"+"|");
+
+            PrinMultiples(100);
+        }
+    
+        private static void PrinMultiples(int MaxNumber){
+            for (int Number=1;Number<=MaxNumber;Number++){
+                
+                    if(Number%15==0){
+                        Console.WriteLine("Numero: "+Number+"|"+" M-3-5"+"|");
+                    }
+                    else if(Number%5==0){
+                        Console.WriteLine("Numero: "+Number+"|"+" M-5"+"|");
+                    }
+                    else if(Number%3==0){
+                        Console.WriteLine("Numero: "+Number+"|"+" M-3"+"|");
+                    }
+                    else{
+                        Console.WriteLine("Numero: "+Number+"|");
+                    }
             }
         }
     }
