f949acdc2338a8ea757dc0f4548020b105db70a1
Julio Robles <jucer74@hotmail.com>
2022-03-20 01:55:58 -0500

    Update Program.cs

--- Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
+++ Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
@@ -1,9 +1,13 @@
-﻿/*
- * Write a program (C#) that prints the numbers from 1 to 100. 
- * But for multiples of three print “M-3” instead of the number and 
- * for the multiples of five print “M-5”. 
- * For numbers which are multiples of both three and five print “M-3-5”
- * */
+﻿/// <summary>
+/// Objetivo:
+///   Imprima los números del 1 a 100, separados por coma.
+///   - Pero para múltiplos de tres imprima “M-3” en lugar del número
+///   - Para los múltiplos de cinco imprima “M-5”
+///   - Para números que son múltiplos de tres y cinco, escriba “M-3-5”
+///   - Para los demas imprima solo el numero.
+///  Ejemplo:
+///   1,2,M-3,4,M-5,M-3,7,8,M-3,M-5,11,M-3,13,14,M-3-5,......
+/// </summary>
 using System;
 
 namespace PrintMultiples
