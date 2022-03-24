8feab24f9302c9c1c4d063a325b399703333e3e6
JCorn39 <88836018+JCorn39@users.noreply.github.com>
2022-03-23 20:46:58 -0500

    Cambios

--- Practices/03.SOLID/1.SRP-Single Responsibility Principle/1.2.Solid.Principles.SRP/ApplicationData.cs
+++ Practices/03.SOLID/1.SRP-Single Responsibility Principle/1.2.Solid.Principles.SRP/ApplicationData.cs
@@ -85,7 +85,7 @@
       }
     }
 
-    /// <summary>
+    /* /// <summary>
     /// Method to generate report
     /// </summary>
     public void GenerateReport(string reportFilename)
@@ -102,7 +102,7 @@
 
       sw.Flush();
       sw.Close();
-    }
+    } */
 
     /// <summary>
     /// Build the Connection String to the database
@@ -116,6 +116,25 @@
       };
 
       return sqlConnectionStringBuilder.ToString();
+    }
+  }
+  public class ReportGenerator
+  {
+    /// <summary>
+    /// Method to generate report
+    /// </summary>
+    public static void GenerateReport(string reportFilename, List<EmployeeDto> employees)
+    {
+      var fullReportFileName = $"{Constants.ReportsPath}{reportFilename}";
+      var sw = new StreamWriter(fullReportFileName);
+
+      foreach (var emp in employees)
+      {
+        sw.WriteLine($"{emp.Id},{emp.FirstName},{emp.LastName},{emp.HireDate},{emp.Email},{emp.Phone}");
+      }
+
+      sw.Flush();
+      sw.Close();
     }
   }
 }
