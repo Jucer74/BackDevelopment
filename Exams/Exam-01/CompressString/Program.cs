d4a14a6c628e333ff275607bfa7ddff4e4dae0ce
Julio Robles <jucer74@hotmail.com>
2022-03-24 09:24:11 -0500

    Notas

--- Exams/Exam-01/CompressString/Program.cs
+++ Exams/Exam-01/CompressString/Program.cs
@@ -1,5 +1,4 @@
-﻿
-/// <summary>
+﻿/// <summary>
 /// Implemente un método para realizar la compresión básica de cadenas utilizando el recuento de caracteres repetidos.
 /// Criterios de Aceptación:
 /// - la cadena aabcccccaaa se convertiría en a2b1c5a3.
@@ -14,148 +13,66 @@ using System;
 
 namespace CompressString
 {
-    public class Program
-    {
+   public class Program
+   {
+      public static void Main()
+      {
+        Compress("aabcccccaaa");
+        // aabcccccaaa => a2b1c5a3
+        Compress("XXXoooxxxOOO");
+        // XXXoooxxxOOO => X3o3x3O3
+        Compress("abbcca");
+        // abbcca => abbcca
+        Compress("aabbcc");
+        // aabbcc => aabbcc
+        Compress("");
+        // Error: the string must not be null or empty
+        Compress("X".PadRight(256, 'X'));
+        // Error: The length of the string must be less than 255 characters.
+        Compress("a1b2c5a3");
+        // Error: Only alphabetic characters [A-Z,a-z] are allowed
+      }
 
-        static int MAXIMO_CARACTERES = 255;
-        public static void Main()
+      private static void Compress(string input)
+      {
+
+        if(input="")
         {
-            Compress("aabcccccaaa");
-            // aabcccccaaa => a2b1c5a3
-            //Compress("XXXoooxxxOOO");
-            // XXXoooxxxOOO => X3o3x3O3
-            //Compress("abbcca");
-            // abbcca => abbcca
-            //Compress("aabbcc");
-            // aabbcc => aabbcc
-            //Compress("");
-            // Error: the string must not be null or empty
-            //Compress("X".PadRight(256, 'X'));
-            // Error: The length of the string must be less than 255 characters.
-            //Compress("a1b2c5a3");
-            // Error: Only alphabetic characters [A-Z,a-z] are allowed
+            Console.WriteLine("Error: the string must not be null or empty");
+        }
+        if(input.Length>250)
+        {
+            Console.WriteLine("Error: The length of the string must be less than 255 charactrs.");
         }
 
-        private static void Compress(string input)
+        cadena = "";
+        counterbyCharacter=0;
+        currentCharacter=input[0];
+        stringBuilder compressedText = new stringBuilder[];
+        
+        for(int currentchar=0; currentchar<input.Length; currentchar++)
         {
-            Char[] listaDeCaracteres = input.ToCharArray();
-
-
-            int posicionInicialDelCaracter = 0;
-            int contadorDePalabraRepetidas = 0;
-
-
-            String cadenaAuxiliar = "";
-            String cadenaComprimida = "";
-
-            if (String.IsNullOrEmpty(input))
+            if(input[currentchar]==input[currentchar++])
             {
-                Console.Write("the string must not be null or empty");
-                return;
+              countCharacter++;
             }
 
-            if (input.Length > MAXIMO_CARACTERES)
-            {
-                Console.Write("The length of the string must be less than 255 characters.");
-                return;
-            }
-
-            if (esAlfabetico(input))
-            {
-                Console.Write("Only alphabetic characters [A-Z,a-z] are allowed.");
-                return;            }
-
-
-            foreach (Char caracter in listaDeCaracteres)
-            {
-
-
-                if (posicionInicialDelCaracter < listaDeCaracteres.Length)
-                {
-
-
-                    if (listaDeCaracteres[posicionInicialDelCaracter].Equals(caracter))
-                    {
-                        contadorDePalabraRepetidas = contadorDePalabraRepetidas + 1;
-                        //Console.Write(listaDeCaracteres[posicionInicialDelCaracter] + "\n");
-                        // Console.Write(caracter + contadorDePalabraRepetidas.ToString() + "\n");
-                        cadenaAuxiliar = caracter.ToString();
-                        //  Console.Write(listaDeCaracteres[posicionInicialDelCaracter]);
-
-
-                        //  Console.Write(contadorDePalabraRepetidas);
-
-                    }
-                    else
-                    {
-
-                        cadenaAuxiliar = cadenaAuxiliar + contadorDePalabraRepetidas.ToString();
-                        cadenaAuxiliar = "";
-                        //cadenaAuxiliar = caracter.ToString() + contadorDePalabraRepetidas.ToString();
-                        contadorDePalabraRepetidas = 0;
-                        Console.Write(contadorDePalabraRepetidas + "\n");
-
-                    }
-                    cadenaComprimida += cadenaAuxiliar;
-                    posicionInicialDelCaracter = posicionInicialDelCaracter + 1;
-                }
-
-
-
-
-
-
-
-
-            }
-
-            Console.Write(cadenaComprimida);
+            Console.WriteLine(cadena);
         }
 
-        public static bool esAlfabetico(string input)
-        {
-            string cadenaEnMinuscula = input.ToLower();
-            foreach (var caracter in cadenaEnMinuscula)
-            {
-                if (!Char.IsLetter(caracter))
-                {
-                    return false;
-                }
-            }
-        }
+      }
 
-    }
+      public static bool isAlphabetic(string input)
+      {
+        string lowerString = input.ToLower();
+        foreach (var character in lowerString)
+        {
+          if(!Char.IsLetter(character))
+          {
+           return false;
+          }
+        }
+          return true;
+      }
+   }
 }
-
-
-/* Char[] listaDeCaracteres = input.ToCharArray();
-
-
-           int posicionInicialDelCaracter = 0;
-           int contadorDePalabraRepetidas = 0;
-
-           String cadenaComprimida = "";
-                               Console.Write(contadorDePalabraRepetidas[0]);
-
-           foreach (Char caracter in listaDeCaracteres)
-           {
-
-               if (caracter == listaDeCaracteres[posicionInicialDelCaracter])
-               {
-                   contadorDePalabraRepetidas += 1;
-               }
-               else
-               {
-                   cadenaComprimida = caracter.ToString() + contadorDePalabraRepetidas.ToString();
-                   contadorDePalabraRepetidas = 0;
-                               Console.Write(contadorDePalabraRepetidas);
-
-
-               }
-
-               posicionInicialDelCaracter += 1;
-
-
-           }
-
-           Console.Write(cadenaComprimida); */
--- Exams/Exam-01/README.md
+++ Exams/Exam-01/README.md
@@ -50,16 +50,16 @@ Error: Only alphabetic characters [A-Z,a-z] are allowed
 RECUERDE SUBIR SU SOLUCIÓN A SU RAMA DE ESTE REPOSITORIO.
 
 # Notas (0.0)
-- No tiene Errores Ni Warnings (0.5)      = 
-- No tiene Codigo Innecesario  (0.5)      = 
-- Funciona y Cumple con el Objetivo (1.5) = 
-- El codigo Es Entendible (1.0)           = 
-- Cumple con el Codigo Limpio (1.5)       = 
+- No tiene Errores Ni Warnings (0.5)      = 0.0
+- No tiene Codigo Innecesario  (0.5)      = 0.0
+- Funciona y Cumple con el Objetivo (1.5) = 0.0
+- El codigo Es Entendible (1.0)           = 0.5
+- Cumple con el Codigo Limpio (1.5)       = 1.0
   Los Nombres de las variables y Funciones: (0.1/ cada una)
   - Revelan la intencion, es decir se sabe que hacen o que almacenan? = OK
   - Los Nombres son claros o son confusos?                            = OK
   - Son Pronunciables                                                 = OK
-  - Son buscables (Numero Magicos o No hay Constantes)?               = OK
+  - Son buscables (Numero Magicos o No hay Constantes)?               = x
   - tiene notaciones innecesarias IntCodigo, strData) ?               = OK
   - Usan Sustantivos para Clases y Verbos para Metodos?               = OK
   - Una sola palabra por concepto?                                    = OK
@@ -68,7 +68,7 @@ RECUERDE SUBIR SU SOLUCIÓN A SU RAMA DE ESTE REPOSITORIO.
   - Usan Datos del dominio, del negocio, problema o solucion ?        = OK
   - Cumplen con el Estandar de Pascal y Camel Case?                   = OK
   Las Funciones                                                         
-  - Son pequeñas y su logica esta bien separada?                      = OK
-  - Las Funciones hacen una sola cosa?                                = OK
-  - Tieen Logica de Retorno directo y correcto o hay If para retornar = OK
-  - No Existen Multiples If anidados o SI hay instrucciones Switch    = OK
+  - Son pequeñas y su logica esta bien separada?                      = x
+  - Las Funciones hacen una sola cosa?                                = x
+  - Tieen Logica de Retorno directo y correcto o hay If para retornar = x
+  - No Existen Multiples If anidados o SI hay instrucciones Switch    = x
