6a580cb39ad4c210c023ec89339a7896a1db296e
JCorn39 <88836018+JCorn39@users.noreply.github.com>
2022-04-06 15:23:40 -0500

    Punto 4

--- Practices/03.SOLID/4.ISP-Interface Segregation Principle/4.2.Solid.Principles.ISP/ReportXML.cs
+++ Practices/03.SOLID/4.ISP-Interface Segregation Principle/4.2.Solid.Principles.ISP/ReportXML.cs
@@ -8,11 +8,11 @@
 
   public class ReportXML : IReportGenerator
   {
-    public void Generate(string reportFilename, List<EmployeeDto> employees)
+    public void Generate(string reportFilename, List<EmployeeData> employees)
     {
       var fullReportFileName = $"{Constants.ReportsPath}{reportFilename}.xml";
       var sw = new StreamWriter(fullReportFileName);
-      XmlSerializer xml = new XmlSerializer(typeof(List<EmployeeDto>));
+      XmlSerializer xml = new XmlSerializer(typeof(List<EmployeeData>));
       xml.Serialize(sw, employees);
       sw.Flush();
       sw.Close();
