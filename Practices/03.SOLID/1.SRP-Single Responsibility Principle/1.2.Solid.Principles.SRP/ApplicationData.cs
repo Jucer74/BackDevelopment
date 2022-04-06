4a3790616be79b86845097d3dc6d4efaa0a0a5b3
JramosF25 <91672832+JramosF25@users.noreply.github.com>
2022-04-06 15:40:18 -0500

    Update ApplicationData.cs

--- Practices/03.SOLID/1.SRP-Single Responsibility Principle/1.2.Solid.Principles.SRP/ApplicationData.cs
+++ Practices/03.SOLID/1.SRP-Single Responsibility Principle/1.2.Solid.Principles.SRP/ApplicationData.cs
@@ -84,30 +84,6 @@
         sqlDatabase.CloseConnection();
       }
     }
-
-    /// <summary>
-    /// Method to generate report
-    /// </summary>
-    /* public void GenerateReport(string reportFilename)
-    {
-      var fullReportFileName = $"{Constants.ReportsPath}{reportFilename}";
-      var sw = new StreamWriter(fullReportFileName);
-
-      var employees = GetEmployees();
-
-      foreach (var emp in employees)
-      {
-        sw.WriteLine($"{emp.Id},{emp.FirstName},{emp.LastName},{emp.HireDate},{emp.Email},{emp.Phone}");
-      }
-
-      sw.Flush();
-      sw.Close();
-    } */
-
-    /// <summary>
-    /// Build the Connection String to the database
-    /// </summary>
-    /// <returns>Connection String</returns>
     private static string GetConnectionString()
     {
       var sqlConnectionStringBuilder = new SQLiteConnectionStringBuilder
@@ -116,25 +92,6 @@
       };
 
       return sqlConnectionStringBuilder.ToString();
-    }
-  }
-    public class ReportGenerator
-  {
-    /// <summary>
-    /// Method to generate report
-    /// </summary>
-    public static void Generate(string reportFilename, List<EmployeeDto> employees)
-    {
-      var fullReportFileName = $"{Constants.ReportsPath}{reportFilename}";
-      var sw = new StreamWriter(fullReportFileName);
-
-      foreach (var emp in employees)
-      {
-        sw.WriteLine($"{emp.Id},{emp.FirstName},{emp.LastName},{emp.HireDate},{emp.Email},{emp.Phone}");
-      }
-
-      sw.Flush();
-      sw.Close();
     }
   }
 }
