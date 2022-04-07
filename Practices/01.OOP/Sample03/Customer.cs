178c212711d8ce7a00ab54562a327d241294f742
JuanJoseEscobar <cheche.juan@hotmail.com>
2022-04-06 21:25:04 -0500

    creacion de example de POO

---
+++ Practices/01.OOP/Sample03/Customer.cs
@@ -0,0 +1,26 @@
+namespace Sample03
+{
+  using System;
+
+  public class Customer: Person
+  {
+    public string AccountNumber { get; set; }
+    public string AccountType { get; set; }
+
+    public Customer():base()
+    {
+        
+    }
+    public Customer(int id
+                    , string firstName
+                    , string lastName
+                    , DateTime dateOfBirth
+                    , char sex
+                    , string accountNumber
+                    , string AccountType):
+    base(id,firstName,lastName,dateOfBirth,sex)
+    {
+        
+    }
+  }
+}
---
+++ Practices/01.OOP/Sample03/Person.cs
@@ -0,0 +1,32 @@
+namespace Sample03
+{
+  using System;
+
+  public class Person
+  {
+    [Required]
+    public int Id { get; private set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public char Sex { get; set; }
+
+    public Person()
+    {
+        
+    }
+
+    public Person(int id, string firstName, string lastName, DateTime dateOfBirth, char sex)
+    {
+        this.Id = id;
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.DateOfBirth = dateOfBirth;
+        this.Sex = sex;
+    }
+
+
+
+
+  }
+}
