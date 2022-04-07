444fd445c17be2c61dd2c999dca65f91fad73c38
Deliox22 <juanfeosorio5a@gmail.com>
2022-04-06 19:42:18 -0500

    sample

---
+++ Practices/01.OOP/Sample01/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Sample01
+{
+    public class Person
+    {
+            private string FirstName;
+            private string LastName;
+            private int Age;
+            private string DateOfBirth;
+            private string PlaceOfBirth;
+            private double Id;
+            private string Address;
+            private string Career;
+            private double PhoneNumber;
+            private double Tall;
+           private double Height;
+
+
+    }
+}
