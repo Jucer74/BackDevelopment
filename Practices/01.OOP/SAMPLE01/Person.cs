ebd969dc963d9f5dbf3270704d13eef7067d0034
oric99 <70826513+oric99@users.noreply.github.com>
2022-04-06 19:44:59 -0500

    HOLA

---
+++ Practices/01.OOP/SAMPLE01/Person.cs
@@ -0,0 +1,19 @@
+/*
+-id:    Cedula/
+        DocumentoIdentidad/
+        PassportId
+-Name:  FirstName/
+        LastName
+-NickName                    -Birth Day              D.D.D (Domain, Driven, Desing)
+-Gender                      -Birth Date
+-Age (ComputeFields)         -Date Of Birth
+-Adress (CompositionFields)  -Place OF Birth
+-Career                      -Nationality
+-PhoneNumber                 -Tall
+-BloodType                   -Weigth
+
+*/
+
+public class Person{
+    
+}
