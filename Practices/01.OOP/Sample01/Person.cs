d0aac7a43c9890ed7b3a1106f678100031c9427d
JramosF25 <juanjorf25@gmail.com>
2022-04-06 19:36:01 -0500

    ClassPerson

---
+++ Practices/01.OOP/Sample01/Person.cs
@@ -0,0 +1,8 @@
+public class Person{
+    private double ID;
+    private string gender;
+    private string name;
+    private string lastName;
+    private DateTime dateOfBirth;
+    private double phoneNumber;
+}
