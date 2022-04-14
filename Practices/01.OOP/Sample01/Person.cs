58a839282b2884aa5369c3121439585c4d82a447
Valgon55 <valgon5500@gmail.com>
2022-04-13 22:48:13 -0500

    TALLER SOLID

---
+++ Practices/01.OOP/Sample01/Person.cs
@@ -0,0 +1,12 @@
+public class Person{
+
+    private int age; //La edad comunmente es un valor que se suele calcular. De todas formas no está mal pedirlo.
+
+    private int idNumber;
+    private double height;
+    private double weight;
+    private string firstName, secondName, lastName, secondlastName;
+    private string gender;
+    private string bloobType; 
+    private string DayOfBorn;
+}
