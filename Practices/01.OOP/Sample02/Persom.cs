a575cbd49b8f6c99915e40b19e12cf4eb43258c2
Deliox22 <juanfeosorio5a@gmail.com>
2022-04-20 18:52:03 -0500

    Ejemplos clase

---
+++ Practices/01.OOP/Sample02/Persom.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sample02
+{
+    public class Person
+    {
+
+
+        //Properties
+        public int Id {get; set;}
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+        public DateTime DateOfBirth {get; set;}
+        public char Sex {get; set;}
+
+        //Constructors
+
+        public Person(int id, string firstName, string lastName,DateTime dateOfBirth, char sex)
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.DateOfBirth = dateOfBirth;
+            this.Sex = sex;
+        }
+        //Methods - Publics
+        //Mewthods - Private
+    }
+}
---
+++ Practices/01.OOP/Sample03/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sample03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
