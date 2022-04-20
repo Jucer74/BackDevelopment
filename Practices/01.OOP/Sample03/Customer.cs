979d70fb5dfc122a805114a6907d990d45f6cb8d
Julio Robles <Jucer74@Hotmail.com>
2022-04-20 18:51:11 -0500

    Samples

--- Practices/01.OOP/Sample03/Customer.cs
+++ Practices/01.OOP/Sample03/Customer.cs
@@ -1,23 +1,23 @@
-public class Customer: Person
+public class Customer : Person
 {
-   public string AccountNumber { get; set; }
-   public string AccountType { get; set; }
+  public string AccountNumber { get; set; }
+  public string AccountType { get; set; }
 
-   public Customer(): base()
-   {
-      
-   }
+  public Customer() : base()
+  {
 
-   public Customer( int id
-                  , string firstName
-                  , string lastNane
-                  , DateTime dateOfBirth
-                  , char sex
-                  , string accoountNumber
-                  , string accountType): 
-         base(id, firstName,lastNane, dateOfBirth, sex)
-   {
-      this.AccountNumber = accoountNumber;
-      this.AccountType = accountType;
-   }
+  }
+
+  public Customer(int id
+                 , string firstName
+                 , string lastNane
+                 , DateTime dateOfBirth
+                 , char sex
+                 , string accoountNumber
+                 , string accountType) :
+        base(id, firstName, lastNane, dateOfBirth, sex)
+  {
+    this.AccountNumber = accoountNumber;
+    this.AccountType = accountType;
+  }
 }
