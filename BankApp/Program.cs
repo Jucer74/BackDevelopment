1e34808276c0881d9d1c8bd93223e3b7b5aece78
heidy1523 <79884394+heidy1523@users.noreply.github.com>
2022-04-20 21:28:14 -0500

    crear el nuevo trabajo

---
+++ BankApp/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class AplicactioBank
+{
+
+    private double clientBalance;
+    private string Account;
+    private string TypeAccount;
+
+    public double Balance
+    {
+
+        get { return clientBalance; }
+
+    }
+
+    public string NumberAccount
+    {
+
+        get { return Account; }
+        set { Account = value; }
+
+    }
+
+    public bool retiro(double amount)
+    {
+        if (clientBalance >= amount)
+        {
+            clientBalance -= amount;
+            return true;
+        }
+        else
+        {
+            console.WriteLine("fondos insuficientes");
+            return false;
+        }
+    }
+
+}
