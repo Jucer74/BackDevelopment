3657cd1264d888b704672d27372fc506d33ecf28
SEVFCA <sevfca246@gmail.com>
2022-04-27 16:45:08 -0500

    BankApp

--- Practices/01.OOP/BankApp/BankAccount.cs
+++ Practices/01.OOP/BankApp/BankAccount.cs
@@ -1,16 +1,38 @@
-namespace BankApp
-{
-using System;
-
 public class BankAccount
-{
-    public int AccountNumber {get;set;}
+        {
+            public int AccountNumber {get;set;}
+            public string PlaceMolder {get;set;}
+            public double BalanceAmount {get;set;}
+            public bool AccountType {get;set;}
 
-    public string PlaceMolder {get;set;}
+            public void Deposit(double amount)
+            {
+                BalanceAmount += amount;
+            }
 
-    public double BalanceAmount {get;set;}
+            public bool Withdrawal(double amount)
+            {
+                if(AccountType == true){
+                    if(BalanceAmount>= amount){
+                        BalanceAmount -= amount;
+                        return true;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    double test = BalanceAmount;
+                    //check that the subtraction does not exceed 1 million
+                    test -= amount;
 
-    public bool AccountType {get;set;}
-}
-}
-
+                    if(test <=-1000001){
+                        return false;
+                    }
+                    else{
+                        BalanceAmount -= amount;
+                        return true;
+                    }
+                }
+            }
+        }
--- Practices/01.OOP/BankApp/Program.cs
+++ Practices/01.OOP/BankApp/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace BankApp
-{
+﻿using System;
+using System.Collections.Generic;
 
-    using System;
+namespace BankApp
+{
+    
     class Program
     {
-        private static void Main(string[] args)
+
+        BankAccount account = new BankAccount();
+        public static List<BankAccount> ListAccounts = new List<BankAccount>();
+        static void Main(string[] args)
         {
             try
         {
@@ -16,8 +21,6 @@
         }
         }
 
-        List <BankAccount> ListAccounts = new List<BankAccount>(); 
-
         private static void Menu()
         {
 
@@ -27,36 +30,62 @@
             {
 
             Console.Clear();
-            Console.WriteLine("     B.A.N.K A.P.P    ");
-            Console.WriteLine("----------------------");
-            Console.WriteLine("1. Create Account");
-            Console.WriteLine("2. Get Balance Account");
-            Console.WriteLine("3. Deposit Account");
-            Console.WriteLine("4. Withdrawal Account");
-            Console.WriteLine("0. Exit");
+            
+            Console.WriteLine("|       B.A.N.K A.P.P       |");
+            Console.WriteLine("|---------------------------|");
+            Console.WriteLine("|    1. Create Account      |");
+            Console.WriteLine("|    2. Get Balance Account |");
+            Console.WriteLine("|    3. Deposit Account     |");
+            Console.WriteLine("|    4. Withdrawal Account  |");
+            Console.WriteLine("|    0. Exit                |");
+            Console.WriteLine("|---------------------------|");
+            
             option = Console.ReadKey().KeyChar;
             Console.WriteLine();
 
             switch (option)
             {
                 case '0':
-                Console.WriteLine("Exit");
+                Console.Clear();
+                Console.WriteLine("[ Exit ]");
                 break;
 
                 case '1':
+                Console.Clear();
+                
+                Console.WriteLine("| C.R.E.A.T.E A.C.C.O.U.N.T |");
+                Console.WriteLine("|---------------------------|");
                 CreateAccount();
+                
                 break;
 
                 case '2':
-                GetBalanceAccount();
+
+                Console.Clear();
+                Console.WriteLine("|    G.E.T B.A.L.A.N.C.E    |");
+                Console.WriteLine("|---------------------------|");
+                GetBalance();
                 break;
 
                 case '3':
+                Console.Clear();
+                
+                Console.WriteLine("|        D.E.P.O.S.I.T      |");
+                Console.WriteLine("|---------------------------|");
+                DepositAccount();
                 break;
-
                 case '4':
+                Console.Clear();
+
+                
+                Console.WriteLine("|    W.I.T.H.D.R.A.W.A.L    |");
+                Console.WriteLine("|---------------------------|");
+                WithdrawalAccount();
                 break;
 
+                default:
+                Console.WriteLine("...Unanswered choice. Try again");
+                break;
             }
 
             Console.WriteLine("\nPress any key to continue... ");
@@ -64,56 +93,90 @@
         }
         }
 
-        private static BankAccount CreateAccount(){
-            Console.Write("Account Number         :");
-            var accountNumber = Console.ReadLine();
-            Console.Write("Place Molder         :");
-            var placeMolder = Console.ReadLine();
-            Console.Write("Balance Amount         :");
-            var balanceAmount = Console.ReadLine();
-            Console.Write("Account Type        :");
-            var accountType = Console.ReadLine();
+        public static void CreateAccount(){
+            BankAccount newAccount = new BankAccount();
 
-            var bankAccount = new BankAccount 
-            {
-                AccountNumber = accountNumber,
-                PlaceMolder = placeMolder,
-                BalanceAmount = balanceAmount,
-                AccountType = accountType
-            };
-
-            if (ListAccounts.Contains<BankAccount>(bankAccount) == false){
-                ListAccounts.Add(bankAccount);
-                Console.WriteLine("Successful account creation");
+            Console.WriteLine("- Enter account number:");
+            int accountNumber = int.Parse(Console.ReadLine());
+            newAccount.AccountNumber = accountNumber;
+            Console.WriteLine("- Enter Position Molder:");
+            string positionMolder = Console.ReadLine();
+            newAccount.PlaceMolder = positionMolder;
+            Console.WriteLine("- Enter Balance Amount:");
+            double balanceAmount = double.Parse(Console.ReadLine());
+            newAccount.BalanceAmount = balanceAmount;
+            Console.WriteLine("- Enter account type:");
+            Console.WriteLine("  1. Savings Account.");
+            Console.WriteLine("  2. Checking Account.");
+            int type = int.Parse(Console.ReadLine());
+            if (type == 1){
+                newAccount.AccountType = true;
             }
             else{
-                Console.WriteLine("Account creation error");
+                newAccount.AccountType = false;
+            }
+            
+            ListAccounts.Add(newAccount);
+            Console.WriteLine("\n.... The account has been created.");
+        }
+
+        public static void GetBalance(){
+            Console.WriteLine("- Enter account number:");
+                int accountNumber = int.Parse(Console.ReadLine());
+                bool flag = false;
+                foreach(BankAccount accountSearch in ListAccounts){
+                if(accountSearch.AccountNumber == accountNumber){
+                    flag=true;
+                    Console.WriteLine("[ Your account balance is: "+accountSearch.BalanceAmount+" ]");
+                    break;
+                }
+            }
+            if(flag == false){
+                Console.WriteLine("\n...Account number does not exist");
             }
         }
 
-        private static bool SearchAccount(BankAccount accountNumberSearch){
-            if (ListAccounts.Contains<BankAccount>(accountNumberSearch)== false){
-                return false;
+        public static void DepositAccount(){
+            Console.WriteLine("- Enter account number:");
+                int accountNumber = int.Parse(Console.ReadLine());
+                bool flag = false;
+                foreach(BankAccount accountSearch in ListAccounts){
+                if(accountSearch.AccountNumber == accountNumber){
+                    flag=true;
+                    Console.WriteLine("- Enter amount to deposit:");
+                    double amount = double.Parse(Console.ReadLine());
+                    accountSearch.Deposit(amount);
+                    Console.WriteLine("\n...Amount deposited");
+                    break;
+                }
             }
-            else{
-                return true;
+            if(flag == false){
+                Console.WriteLine("\n...Account number does not exist");
             }
         }
 
-        private static double GetBalanceAccount(){
-            Console.WriteLine("Enter account number");
-            var accountNumberSearch = Console.ReadLine();
-            accountNumberSearch = new BankAccount();
-            if(SearchAccount(accountNumberSearch)==true){
-                Console.WriteLine("error looking for the account");
+        public static void WithdrawalAccount(){
+            Console.WriteLine("- Enter account number:");
+            int accountNumber = int.Parse(Console.ReadLine());
+            bool flag = false;
+            foreach(BankAccount accountSearch in ListAccounts){
+                if(accountSearch.AccountNumber == accountNumber){
+                    flag=true;
+                    Console.WriteLine("- Enter amount to withdraw:");
+                    double amount = double.Parse(Console.ReadLine());
+                    if (accountSearch.Withdrawal(amount)==true){
+                        Console.WriteLine("\n...the amount was withdrawn");
+                    }
+                    else{
+                        Console.WriteLine("\n...The money could not be withdrawn");
+                    }
+                    break;
+                }
             }
-            else{
-                Console.WriteLine("Found account");
-                Console.WriteLine("Your Balance is:      ");
-                Console.WriteLine(accountNumberSearch.BalanceAmount());
+            if(flag == false){
+                Console.WriteLine("\n...Account number does not exist");
             }
         }
 
     }
-
 }
