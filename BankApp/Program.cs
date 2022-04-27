2724830a7ea6a312eb61cf57133a8a2bd043a4a4
heidy1523 <79884394+heidy1523@users.noreply.github.com>
2022-04-27 16:49:35 -0500

    Update Program.cs

--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -1,38 +1,192 @@
-﻿// See https://aka.ms/new-console-template for more information
-public class AplicactioBank
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace BankApp
 {
-
-    private double clientBalance;
-    private string Account;
-    private string TypeAccount;
-
-    public double Balance
-    {
-
-        get { return clientBalance; }
-
-    }
-
-    public string NumberAccount
-    {
-
-        get { return Account; }
-        set { Account = value; }
-
-    }
-
-    public bool retiro(double amount)
-    {
-        if (clientBalance >= amount)
+    class Program
+    {     
+        public static List<AccountDto> listAccount = new List<AccountDto>();    
+        private static void Main(string[] args)
         {
-            clientBalance -= amount;
-            return true;
+           try
+           {
+               Menu();
+           }
+           catch(Exception e)
+           {
+               Console.WriteLine(e);
+           }
+
         }
-        else
+        private static void Menu()
         {
-            console.WriteLine("fondos insuficientes");
-            return false;
+            var option =' ';
+
+
+            while(option !='0')
+            {
+                Console.Clear();  
+                Console.Clear();
+                Console.WriteLine("     Welcone    ");
+                Console.WriteLine("------------------------------");
+                Console.WriteLine("1. Create Account");
+                Console.WriteLine("2. Deposit Account");
+                Console.WriteLine("3. withdrawal Account");
+                Console.WriteLine("4. Balance Account");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("Select Option:");
+                option = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                switch(option)
+                {
+                    case '0':
+
+                        Console.WriteLine("Exit");
+                        break;
+
+                    case '1':
+                       
+                        CreateAccount();
+                        break;
+
+                    case '2':
+
+                        GetBalanceAccount();
+                        break;
+
+                    case '3':
+                       
+                        DepositAccount();
+                        break;
+
+                    case '4':
+                        
+                        WithdrawalAccount();
+                        
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+                }
+
+            }
+        }
+        private static void  CreateAccount()
+        {
+            
+            var option = ' ';
+            var accountType = " ";
+            
+            Console.WriteLine("option 1 --------> CreateAccount");
+
+            Console.WriteLine("Account Number: ");
+            int accountNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Place Holder: ");
+            var placeHolder = Console.ReadLine();
+
+            Console.WriteLine("\n\n Account Type");
+            Console.WriteLine("1. Saving");
+            Console.WriteLine("2. Checking\n");
+            option = Console.ReadKey().KeyChar;
+
+            switch (option)
+            {
+                case '1':
+
+                    accountType = "Saving";
+                    break;
+
+                case '2':
+
+                    accountType = "Checking";
+                    break;
+
+                default:
+                    Console.WriteLine("Account type not found");
+                    break;
+            }
+        }
+        private static AccountDto CreateAccount(int accountNumber, 
+                                                string placeHolder, 
+                                                int balanceAccount, 
+                                                string accountType)
+        {
+           
+            AccountDto accountDto = new AccountDto();
+
+            accountDto.Accountnumber = accountNumber;
+            accountDto.Placeholder = placeHolder;
+            accountDto.Balanceaccount = balanceAccount;
+            accountDto.Accountype = accountType;
+
+        return accountDto;
+        }
+        private static void GetBalanceAccount()
+        {
+            Console.WriteLine("option 1 --------> Balance");
+
+            Console.WriteLine("Account number: ");
+            int accountNumber = int.Parse(Console.ReadLine());
+            
+            foreach (AccountDto Ac in listAccount)
+            {
+                if(Ac.Accountnumber == accountNumber){
+
+                    Console.Write($"place Holder {Ac.Placeholder} ");
+
+                    Console.Write($"Balance account:{ Ac.Balanceaccount}");
+                   
+                }
+
+            }
+
+        }
+
+        private static void WithdrawalAccount()
+        {
+              Console.WriteLine("option 1 --------> Withdrawal");
+
+            Console.Write("Account number: ");
+            int NumberA = int.Parse(Console.ReadLine());
+            
+            foreach (AccountDto Ac in listAccount)
+            {
+                if(Ac.NumberA == NumberA){
+                    Console.Write($"Balance account:{Ac.Balanceaccount} ");
+                
+                    Console.Write("Enter the balance to withdraw: ");
+                    int balance = int.Parse(Console.ReadLine());
+
+                    Ac.Withdrawal(balance);
+                }
+
+            }
+
+        }
+        private static void DepositAccount()
+        {
+             Console.WriteLine("option 1 --------> Deposit");
+
+            Console.Write("\nAccount number: ");
+            int accountNumber = int.Parse(Console.ReadLine());
+            
+            foreach (AccountDto Ac in listAccount)
+            {
+                if (Ac.Accountnumber == accountNumber){
+                  
+                    Console.WriteLine(Ac.Placeholder);
+                    Console.Write($"Your  balance is: {Ac.Balanceaccount}");
+                
+
+                    Console.Write("\nWhat balance do you want to deposit: ");
+                    int balanceAccount = int.Parse(Console.ReadLine());
+
+                    Ac.Deposit(balanceAccount);
+                }
+
+            }
         }
     }
-
 }
