384a390b2b2b8795497d0ae6f18d345006b0cef4
nizecode <nicolas1106sevilla@gmail.com>
2022-04-26 19:52:39 -0500

    bank

---
+++ Practices/01.OOP/Bank/BankApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
