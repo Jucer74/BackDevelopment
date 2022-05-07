0b104486cf8b5135a859e876a86c1524d208eef8
Julio Robles <Jucer74@Hotmail.com>
2022-05-07 05:35:16 -0500

    Update Bank.cs

--- Exams/Exam-02/BankApp/Bank.cs
+++ Exams/Exam-02/BankApp/Bank.cs
@@ -3,101 +3,103 @@ using System.Collections.Generic;
 
 namespace BankApp
 {
-    public class Bank
-    {
-        private static List<BankAccount> accountList;
+   public class Bank
+   {
+       private static List<BankAccount> accountList;
+//      private static List<BankAccount> accountList = new List<BankAccount>(); //Solucion: asi se inicializa solo una vez la lista,
 
-        private static TransactionAccount transactionAccount;
+      private static TransactionAccount transactionAccount;
 
-        private static UtilityAccountValidation utilityAccountValidation;
+      private static UtilityAccountValidation utilityAccountValidation;
 
-        private static void Main(string[] args)
-        {
-            try
-            {
-                Menu();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-        }
+      // Por que no hay Clase Program??
+      private static void Main(string[] args) 
+      {
+         try
+         {
+            Menu();
+         }
+         catch (Exception e)
+         {
+            Console.WriteLine(e);
+         }
+      }
 
+      private static void InsertAccount() //Por que esta clase no esta en transaccional?
+      {
+         utilityAccountValidation = new UtilityAccountValidation();
+         Console.Clear();
+         Console.WriteLine("Insert new Account");
+         Console.WriteLine("-------------------");
+         Console.WriteLine();
 
-        private static void InsertAccount()
-        {
-            utilityAccountValidation = new UtilityAccountValidation();
+         var account = utilityAccountValidation.SelectAccountType(accountList);
+         accountList = new List<BankAccount>(); //EROR: Recreea la Lista cada vez que se Inserta una Cuenta
+         accountList.Add(account);
+
+         if (accountList != null)
+         {
+            Console.WriteLine("\nAccount Created successfully\n");
+         }
+         else
+         {
+            Console.WriteLine("\nError created account\n");
+         }
+      }
+
+      private static void Menu()
+      {
+         var option = ' ';
+         while (option != '0')
+         {
             Console.Clear();
-            Console.WriteLine("Insert new Account");
-            Console.WriteLine("-------------------");
+            Console.WriteLine("    Select an Operation to Perform    ");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("1. Create Account");
+            Console.WriteLine("2. Get Balance Account");
+            Console.WriteLine("3. Deposit Account");
+            Console.WriteLine("4. Withdrawal Account");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("Select Option:");
+            option = Console.ReadKey().KeyChar;
             Console.WriteLine();
-
-            var account = utilityAccountValidation.SelectAccountType(accountList);
-            accountList = new List<BankAccount>();
-            accountList.Add(account);
-
-            if (accountList != null)
+            transactionAccount = new TransactionAccount();
+            switch (option)
             {
-                Console.WriteLine("\nAccount Created successfully\n");
+               case '0':
+                  Console.WriteLine("Exit");
+                  break;
+
+               case '1':
+                  InsertAccount();
+                  break;
+
+               case '2':
+                  transactionAccount.GetBalance(accountList);
+                  break;
+
+               case '3':
+                  transactionAccount.Deposit(accountList);
+                  break;
+
+               case '4':
+                  transactionAccount.Withdrawal(accountList);
+                  break;
+
+               default:
+                  Console.WriteLine("Invalid Option");
+                  break;
             }
-            else
-            {
-                Console.WriteLine("\nError created account\n");
-            }
-        }
 
-        private static void Menu()
-        {
-        var option = ' ';
-            while (option != '0')
-            {
-                Console.Clear();
-                Console.WriteLine("    Select an Operation to Perform    ");
-                Console.WriteLine("------------------------------");
-                Console.WriteLine("1. Create Account");
-                Console.WriteLine("2. Get Balance Account");
-                Console.WriteLine("3. Deposit Account");
-                Console.WriteLine("4. Withdrawal Account");
-                Console.WriteLine("0. Exit");
-                Console.WriteLine("Select Option:");
-                option = Console.ReadKey().KeyChar;
-                Console.WriteLine();
-                transactionAccount = new TransactionAccount();
-                switch (option)
-                {
-                    case '0':
-                        Console.WriteLine("Exit");
-                        break;
+           ;
 
-                    case '1':
-                        InsertAccount();
-                        break;
+            Console.WriteLine("\nPress any key to continue... ");
+            Console.ReadKey();
+         }
+      }
 
-                    case '2':
-                        transactionAccount.GetBalance(accountList);
-                        break;
-
-                    case '3':
-                        transactionAccount.Deposit(accountList);
-                        break;
-
-                    case '4':
-                        transactionAccount.Withdrawal(accountList);
-                        break;
-
-                    default:
-                        Console.WriteLine("Invalid Option");
-                        break;
-                }
-
-              ;
-
-                Console.WriteLine("\nPress any key to continue... ");
-                Console.ReadKey();
-            }
-        }
-        public Bank()
-        {
-        }
-    }
+      public Bank()
+      {
+      }
+   }
 }
