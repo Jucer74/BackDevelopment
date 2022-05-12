db10e82d05aa00e38e80c6840b7d1a0a984ae207
andresfbastidas <andresfbastidas@hotmail.com>
2022-05-11 21:17:42 -0500

    net bank api

---
+++ Practices/08.API-Rest/NetBank.Api/Controllers/CreditCardController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NetBank.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CreditCardController : ControllerBase
+    {
+       
+        private readonly ILogger<CreditCardController> _logger;
+
+        public CreditCardController(ILogger<CreditCardController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
