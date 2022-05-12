709bc0ef96b0a7dc7ed21c8e66837d2f87d1f23b
XMunteR <danielal19022003@gmail.com>
2022-05-12 10:46:49 -0500

    CreditCard Api

---
+++ Practices/08.API-Rest/NetBank.api/Controllers/CreditCardController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NetBank.api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CreditCardController : ControllerBase
+    {
+        private readonly ILogger<CreditCardController> _logger;
+
+        public CreditCardController(ILogger<CreditCardController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            
+            return Ok();
+        }
+    }
+}
