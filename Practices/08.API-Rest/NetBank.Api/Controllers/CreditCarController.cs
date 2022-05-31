9d911be7a779bd61e75f5acafc2b4474a3fa92bb
JuanJoseEscobar <cheche.juan@hotmail.com>
2022-05-30 21:24:34 -0500

    Actualizacion Api

--- Practices/08.API-Rest/NetBank.Api/Controllers/CreditCarController.cs
+++ Practices/08.API-Rest/NetBank.Api/Controllers/CreditCarController.cs
@@ -11,19 +11,31 @@ namespace NetBank.Api.Controllers
     [Route("[controller]")]
     public class CreditCarController : ControllerBase
     {
-        
-
-        private readonly ILogger<CreditCarController> _logger;
-
-        public CreditCarController(ILogger<CreditCarController> logger)
+        private readonly ILogger<CreditCardController> _logger;
+        private readonly CreditCardService _creditCardService;
+        public CreditCardController(CreditCardService creditCardService, ILogger<CreditCardController> logger)
         {
             _logger = logger;
+            _creditCardService = creditCardService;
         }
 
-        [HttpGet]
-        public IActionResult Get()
+        [HttpGet("{creditcardNumber}")]
+        public IActionResult Get(string creditcardNumber)
         {
-          return Ok();  
+            var validateResult = _creditCardService.Validate(creditcardNumber);
+            var result = _creditCardService.Result;
+
+            switch (validateResult)
+            {
+                case ValidationResultType.Ok:
+                    return Ok(result);     
+                case ValidationResultType.BadRequest:
+                    return BadRequest(result);     
+                case ValidationResultType.NotFound:
+                    return NotFound(result);     
+                default:
+                    return StatusCode(StatusCodes.Status500InternalServerError, new CreditCardResult("Internal Server Error", false));
+            }
         }
     }
 }
