8d2c4a0a0103e7ddb949eb687997946685221457
jhon03 <hoyosb90@gmail.com>
2022-06-01 15:56:01 -0500

    tareafinal

---
+++ Practices/08.API-Rest/NetBank.Api/Controllers/creditCardController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NetBank.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CreditCardController : ControllerBase
+    {
+       
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+           return OK();
+        }
+    }
+}
