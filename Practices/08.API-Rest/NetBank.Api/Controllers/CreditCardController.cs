c43571eb6a6bbf5089d7520c061d5127acd96ef6
andresfbastidas <andresfbastidas@hotmail.com>
2022-06-01 19:04:19 -0500

    fixes

--- Practices/08.API-Rest/NetBank.Api/Controllers/CreditCardController.cs
+++ Practices/08.API-Rest/NetBank.Api/Controllers/CreditCardController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using NetBank.Api.Define;
+using NetBank.Api.Models;
+using NetBank.Api.Services;
 
 namespace NetBank.Api.Controllers
 {
@@ -8,18 +11,31 @@ namespace NetBank.Api.Controllers
     [Route("[controller]")]
     public class CreditCardController : ControllerBase
     {
-       
         private readonly ILogger<CreditCardController> _logger;
-
-        public CreditCardController(ILogger<CreditCardController> logger)
+        private readonly CreditCardService _creditCardService;
+        public CreditCardController(CreditCardService creditCardService, ILogger<CreditCardController> logger)
         {
             _logger = logger;
+            _creditCardService = creditCardService;
         }
 
-        [HttpGet]
-        public IActionResult Get()
+        [HttpGet("{creditcardNumber}")]
+        public IActionResult Get(string creditcardNumber)
         {
-            return Ok();
+            var validateResult = _creditCardService.Validate(creditcardNumber);
+            var result = _creditCardService.Result;
+
+            switch (validateResult)
+            {
+                case ValidationResultType.Ok:
+                    return Ok(result);     
+                case ValidationResultType.BadRequest:
+                    return BadRequest(result);     
+                case ValidationResultType.NotFound:
+                    return NotFound(result);     
+                default:
+                    return StatusCode(StatusCodes.Status500InternalServerError, new CreditCardResult("Internal Server Error", false));
+            }
         }
     }
 }
