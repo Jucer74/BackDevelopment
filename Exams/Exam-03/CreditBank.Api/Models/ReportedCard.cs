c3698d270227c503e342e7c54cbadbe3d91716d7
JuanJoseEscobar <cheche.juan@hotmail.com>
2022-06-04 17:12:11 -0500

    Modificacion Clase  ReportedCard

--- Exams/Exam-03/CreditBank.Api/Models/ReportedCard.cs
+++ Exams/Exam-03/CreditBank.Api/Models/ReportedCard.cs
@@ -2,16 +2,26 @@
 {
    public class ReportedCard
    {
+      [Key]
       public int Id { get; set; }
+
+      [Required(ErrorMessage = "IssuingNetwork is required")]
       public string IssuingNetwork { get; set; }
+
+      [Required(ErrorMessage = "CreditCardNumber is required")]
       public string CreditCardNumber { get; set; }
+
       public string FirstName { get; set; }
       public string LastName { get; set; }
-      public string StatusCard { get; set; }
-      public DateTime ReportedDate { get; set; }
-      public LastUpdatedDate LastUpdatedDate { get; set; }
 
-      
+      [Required(ErrorMessage = "StatusCard is required")]
+      public string StatusCard { get; set; }
+
+      [Required(ErrorMessage = "ReportedDate is required")]
+      public DateTime ReportedDate { get; set; }
+
+      [Required(ErrorMessage = "LastUpdatedDate is required")]
+      public DateTime LastUpdatedDate { get; set; }
 
    }
 }
