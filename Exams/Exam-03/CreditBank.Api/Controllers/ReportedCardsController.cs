d4cec60ac69e80985515340c7381f7430575dae8
JuanJoseEscobar <cheche.juan@hotmail.com>
2022-06-04 18:06:47 -0500

    Modificacion de la clase controladora

--- Exams/Exam-03/CreditBank.Api/Controllers/ReportedCardsController.cs
+++ Exams/Exam-03/CreditBank.Api/Controllers/ReportedCardsController.cs
@@ -1,11 +1,25 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using CreditBank.Api.Models;
+using CreditBank.Api.Services;
 
 namespace CreditBank.Api.Controllers
 {
-   [Route("api/[controller]")]
+   [Route("api/v1.0/[controller]")]
    [ApiController]
    public class ReportedCardsController : ControllerBase
    {
+      private readonly ReportedCardService _reportedCardService;
+      public ReportedCardsController(ReportedCardService reportedCardService)
+      {
+         _reportedCardService = reportedCardService;
+      }
+
+      // GET: api/v1.0/<ReportedCardsController>
+      [HttpGet]
+      public async Task<ActionResult<IList<ReportedCard>>> GetAllReportedCards()
+      {
+         return Ok(await _reportedCardService.GetAllReportedCards());
+      }
    }
 }
