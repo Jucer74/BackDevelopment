5b65c4421bea755f44d63ee67d609d96ec91f010
JuanJoseEscobar <cheche.juan@hotmail.com>
2022-06-04 17:37:37 -0500

    Creacion de AppDbcontext

--- Exams/Exam-03/CreditBank.Api/DataAccess/AppDbContext.cs
+++ Exams/Exam-03/CreditBank.Api/DataAccess/AppDbContext.cs
@@ -1,6 +1,17 @@
-﻿namespace CreditBank.Api.DataAccess
+﻿using CreditBank.Api.Models;
+
+namespace CreditBank.Api.DataAccess
 {
-   public class AppDbContext
+   public class AppDbContext : DbContext
    {
+      public AppDbContext()
+      {
+      }
+
+      public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+      {
+      }
+
+      public DbSet<ReportedCard> ReportedCards { get; set; }
    }
 }
