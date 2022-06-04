08452fc68f586ef71527aafa4349f3bce49bed94
JuanJoseEscobar <cheche.juan@hotmail.com>
2022-06-04 16:45:46 -0500

    creacion reportedcard

--- Exams/Exam-03/CreditBank.Api/Models/ReportedCard.cs
+++ Exams/Exam-03/CreditBank.Api/Models/ReportedCard.cs
@@ -2,5 +2,16 @@
 {
    public class ReportedCard
    {
+      public int Id { get; set; }
+      public string IssuingNetwork { get; set; }
+      public string CreditCardNumber { get; set; }
+      public string FirstName { get; set; }
+      public string LastName { get; set; }
+      public string StatusCard { get; set; }
+      public DateTime ReportedDate { get; set; }
+      public LastUpdatedDate LastUpdatedDate { get; set; }
+
+      
+
    }
 }
