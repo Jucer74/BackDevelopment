ad67f4fee05121707a2a426a55a59580e5ca4c39
XMunteR <danielal19022003@gmail.com>
2022-06-06 18:10:50 -0500

    EXAM-03

---
+++ Exams/Exam-03/CreditBank.Api/Controllers/CreditCardController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreditBank.Api.Controllers
+{
+   [Route("api/v1.0/[controller]")]
+   [ApiController]
+   public class CreditCardController : ControllerBase
+   {
+   }
+}
