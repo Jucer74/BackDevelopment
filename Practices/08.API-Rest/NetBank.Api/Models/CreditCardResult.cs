9474c1be04727134264c64b16ecbe66a17ac98de
nicolas2199 <niko2118@hotmail.com>
2022-06-06 00:11:54 -0500

    Entrega

---
+++ Practices/08.API-Rest/NetBank.Api/Models/CreditCardResult.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace NetBank.Api.Models
+{
+
+   public class CreditCardResult
+   {
+      public string IssuingNetwork { get; set; }
+      public bool Valid { get; set; }
+
+      public CreditCardResult(string issuingNetwork, bool valid)
+      {
+         IssuingNetwork = issuingNetwork;
+         Valid = Valid;
+      }
+   }
+}
