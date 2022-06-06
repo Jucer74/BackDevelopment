36fc9a363a8cff9f13a3082595cd04fa95094bc0
Mariadelmarher <mareshernandez1@gmail.com>
2022-06-05 23:05:47 -0500

    siguiendo con el projectt

--- Exams/Exam-03/CreditBank.Api/Controllers/CreditCardController.cs
+++ Exams/Exam-03/CreditBank.Api/Controllers/CreditCardController.cs
@@ -1,5 +1,7 @@
-using Microsoft.AspNetCore.Http;
+using CreditBank.Api.Utilities;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
 
 namespace CreditBank.Api.Controllers
 {
@@ -7,5 +9,28 @@ namespace CreditBank.Api.Controllers
     [ApiController]
     public class CreditCardController : ControllerBase
     {
+        // GET api/v1.0/CreditCard/CheckDigitStatus/{creditCardNumber}
+        [HttpGet("CheckDigitStatus/{creditCardNumber}")]
+        public ActionResult<string> GetCreditCardCheckDigitStatus(string creditCardNumber)
+        {
+            try
+            {
+                if (!CreditCardValidator.IsNumericCard(creditCardNumber))
+                {
+                    return BadRequest($"Credit Card [{creditCardNumber}] is NOT Numeric");
+                }
+
+                if (CreditCardValidator.IsValid(creditCardNumber))
+                {
+                    return Ok($"Credit Card [{creditCardNumber}] is Valid");
+                }
+
+                return Ok($"Credit Card [{creditCardNumber}] is NOT Valid");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
     }
 }
--- Exams/Exam-03/CreditBank.Api/DataAccess/ReportedCardDataAccess.cs
+++ Exams/Exam-03/CreditBank.Api/DataAccess/ReportedCardDataAccess.cs
@@ -1,6 +1,39 @@
-﻿namespace CreditBank.Api.DataAccess
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using CreditBank.Api.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace CreditBank.Api.DataAccess
 {
-   public class ReportedCardDataAccess
-   {
-   }
+    public class ReportedCardDataAccess
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ReportedCardDataAccess(AppDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task<IList<ReportedCard>> GetAllReportedCards()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IList<ReportedCard>> GetAllReportedCardsByIssuingNetworkName(string issuingNetworkName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ReportedCard> GetReportedCard(string creditCardNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> PutCreditCardReactivated(string creditCardNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
