3302440de7d944288653d3bbf17d30ff2bd85c6a
Mariadelmarher <mareshernandez1@gmail.com>
2022-06-05 22:09:13 -0500

    creacion, correccion y modificacion del examen

---
+++ Exams/Exam-03/CreditBank.Api/Controllers/ReportedCardsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using CreditBank.Api.Models;
+using CreditBank.Api.Services;
+using System.Collections.Generic;
+namespace CreditBank.Api.Controllers
+
+
+{
+    [Route("api/v1.0/[controller]")]
+    [ApiController]
+    public class ReportedCardsController : ControllerBase
+    {
+        private readonly ILogger<ReportedCardsController> _logger;
+        private readonly ReportedCardService _reportedCardService;
+
+        public ReportedCardsController(ReportedCardService reportedCardService, ILogger<ReportedCardsController> logger)
+        {
+            _logger = logger;
+            _reportedCardService = reportedCardService;
+        }
+
+        // GET: api/v1.0/<ReportedCardsController>
+        [HttpGet]
+        public async Task<ActionResult<IList<ReportedCard>>> GetAllReportedCards()
+        {
+            return Ok(await _reportedCardService.GetAllReportedCards());
+        }
+
+        // GET: api/v1.0/ReportedCards/IssuingNetwork
+        [HttpGet("{issuingNetworkName}")]
+        public async Task<ActionResult<IList<ReportedCard>>> GetAllReportedCardsByIssuingNetworkName(string issuingNetworkName)
+        {
+            return Ok(await _reportedCardService.GetAllReportedCardsByIssuingNetworkName(issuingNetworkName));
+        }
+
+        // GET: api/v1.0/ReportedCards/creditCardNumber
+        [HttpGet("{creditCardNumber}")]
+        public async Task<ActionResult<ReportedCard>> GetReportedCard(string creditCardNumber)
+        {
+            return Ok(await _reportedCardService.GetReportedCard(creditCardNumber));
+        }
+
+        // Put: api/v1.0/ReportedCards/IssuingNetworkassadsad
+        [HttpPut("{creditCardNumber}")]
+        public async Task<ActionResult<string>> PutCreditCardReactivated(string creditCardNumber)
+        {
+            return Ok(await _reportedCardService.PutCreditCardReactivated(creditCardNumber));
+        }
+    }
+}
---
+++ Exams/Exam-03/CreditBank.Api/Services/ReportedCardService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using CreditBank.Api.Models;
+using CreditBank.Api.DataAccess;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace CreditBank.Api.Services
+{
+    public class ReportedCardService
+    {
+        private readonly ReportedCardDataAccess _reportedCardDataAccess;
+
+        public ReportedCardService(ReportedCardDataAccess reportedCardDataAccess)
+        {
+            _reportedCardDataAccess = reportedCardDataAccess;
+        }
+
+        public async Task<IList<ReportedCard>> GetAllReportedCards()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IList<ReportedCard>> GetAllReportedCardsByIssuingNetworkName(string issuingNetworkName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ReportedCard> GetReportedCard(string creditCardNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> PutCreditCardReactivated(string creditCardNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
