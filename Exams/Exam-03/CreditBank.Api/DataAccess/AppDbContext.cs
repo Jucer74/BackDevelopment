d9af7c3b0ba15b1fe7ca6c6709848f6ae31b7a32
XMunteR <danielal19022003@gmail.com>
2022-06-06 19:43:40 -0500

    me toco salir F

--- Exams/Exam-03/CreditBank.Api/DataAccess/AppDbContext.cs
+++ Exams/Exam-03/CreditBank.Api/DataAccess/AppDbContext.cs
@@ -1,6 +1,19 @@
-﻿namespace CreditBank.Api.DataAccess
+﻿using System;
+using CreditBank.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CreditBank.Api.DataAccess
 {
-   public class AppDbContext
-   {
-   }
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ReportedCard> ReportedCards { get; set; }
+    }
 }
