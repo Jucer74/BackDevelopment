7c1af5926c15310120de7f2cabc79a02b6c0ffda
Danicubo <89595391+Danicubo@users.noreply.github.com>
2022-06-06 21:49:37 -0500

    Update BadRequestException.cs

--- Exams/Exam-03/CreditBank.Api/Exceptions/BadRequestException.cs
+++ Exams/Exam-03/CreditBank.Api/Exceptions/BadRequestException.cs
@@ -1,30 +1,29 @@
-﻿using System;
+using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.Serialization;
 
 namespace CreditBank.Api.Exceptions
 {
-   [ExcludeFromCodeCoverage]
-   [Serializable]
-   public class BadRequestException : BusinessException
-   {
-      public BadRequestException()
-      {
-      }
+    [ExcludeFromCodeCoverage]
+    [Serializable]
+    public class BadRequestException : BusinessException
+    {
+        public BadRequestException()
+        {
+        }
 
-      public BadRequestException(string message) : base(message)
-      {
-      }
+        public BadRequestException(string message) : base(message)
+        {
+        }
 
-      public BadRequestException(string message, Exception innerException)
-          : base(message, innerException)
-      {
-      }
+        public BadRequestException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
 
-      // Without this constructor, deserialization will fail
-      protected BadRequestException(SerializationInfo info, StreamingContext context)
-          : base(info, context)
-      {
-      }
-   }
+        protected BadRequestException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
 }
