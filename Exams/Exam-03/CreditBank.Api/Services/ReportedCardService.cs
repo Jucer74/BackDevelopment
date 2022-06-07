3ca4dbeaba1290e5614bda021f4db6bb71741b11
nicolas2199 <niko2118@hotmail.com>
2022-06-06 20:16:50 -0500

    validacion

--- Exams/Exam-03/CreditBank.Api/Services/ReportedCardService.cs
+++ Exams/Exam-03/CreditBank.Api/Services/ReportedCardService.cs
@@ -1,29 +1,58 @@
-﻿public class ReportedCardService
-  {
-  private readonly ReportedCardDataAccess _reportedCardDataAccess;
+﻿using System;
+using CreditBank.Api.Models;
+using CreditBank.Api.DataAccess;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using CreditBank.Api.Exceptions;
 
-  public ReportedCardBL(ReportedCardDataAccess reportedCardDataAccess)
-  {
-    _reportedCardDataAccess = reportedCardDataAccess;
-  }
+namespace CreditBank.Api.Services
+{
+    public class ReportedCardService
+    {
+        private readonly ReportedCardDataAccess _reportedCardDataAccess;
 
-  public async Task<IList<ReportedCard>> GetAllReportedCards()
-  {
-    throw new NotImplementedException();
-  }
+        public ReportedCardService(ReportedCardDataAccess reportedCardDataAccess)
+        {
+            _reportedCardDataAccess = reportedCardDataAccess;
+        }
 
-  public async Task<IList<ReportedCard>> GetAllReportedCardsByIssuingNetworkName(string issuingNetworkName)
-  {
-    throw new NotImplementedException();
-  }
+        public async Task<IList<ReportedCard>> GetAllReportedCards()
+        {
+            var reportedCardList = await _reportedCardDataAccess.GetAllReportedCards();
 
-  public async Task<ReportedCard> GetReportedCard(string creditCardNumber)
-  {
-    throw new NotImplementedException();
-  }
+            return reportedCardList;
+        }
 
-  public async Task<string> PutCreditCardReactivated(string creditCardNumber)
-  {
-    throw new NotImplementedException();
-  }
+        public async Task<IList<ReportedCard>> GetAllReportedCardsByIssuingNetworkName(string issuingNetworkName)
+        {
+            var reportedCardList = await _reportedCardDataAccess.GetAllReportedCardsByIssuingNetworkName(issuingNetworkName);
+
+            if (reportedCardList == null || reportedCardList.Count == 0)
+            {
+                throw new NotFoundException($"{issuingNetworkName} Not Found");
+            }
+
+            return reportedCardList;
+        }
+        
+        public async Task<ReportedCard> GetReportedCard(string creditCardNumber)
+        {
+            var reportedCard = await _reportedCardDataAccess.GetReportedCard(creditCardNumber);
+            if (reportedCard == null)
+            {
+                throw new NotFoundException($"{creditCardNumber} is not found");
+            }
+
+            return reportedCard;
+        }
+
+        public async Task<string> PutCreditCardReactivated(string creditCardNumber)
+        {
+            await GetReportedCard(creditCardNumber);
+            var messageUpdateCreditCard = await _reportedCardDataAccess.PutCreditCardReactivated(creditCardNumber);
+
+            return messageUpdateCreditCard;
+        }
+    }
 }
