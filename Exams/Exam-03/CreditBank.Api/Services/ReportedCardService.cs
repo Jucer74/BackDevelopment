ab52f259b6443ebbd01beddbd9564924c513d50c
nicolas2199 <niko2118@hotmail.com>
2022-06-06 19:47:13 -0500

    final

--- Exams/Exam-03/CreditBank.Api/Services/ReportedCardService.cs
+++ Exams/Exam-03/CreditBank.Api/Services/ReportedCardService.cs
@@ -1,6 +1,29 @@
-﻿namespace CreditBank.Api.Services
-{
-   public class ReportedCardService
-   {
-   }
+﻿public class ReportedCardService
+  {
+  private readonly ReportedCardDataAccess _reportedCardDataAccess;
+
+  public ReportedCardBL(ReportedCardDataAccess reportedCardDataAccess)
+  {
+    _reportedCardDataAccess = reportedCardDataAccess;
+  }
+
+  public async Task<IList<ReportedCard>> GetAllReportedCards()
+  {
+    throw new NotImplementedException();
+  }
+
+  public async Task<IList<ReportedCard>> GetAllReportedCardsByIssuingNetworkName(string issuingNetworkName)
+  {
+    throw new NotImplementedException();
+  }
+
+  public async Task<ReportedCard> GetReportedCard(string creditCardNumber)
+  {
+    throw new NotImplementedException();
+  }
+
+  public async Task<string> PutCreditCardReactivated(string creditCardNumber)
+  {
+    throw new NotImplementedException();
+  }
 }
