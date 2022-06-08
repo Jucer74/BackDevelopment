43de1a9e345b2c57fbf2c699647e6de8460f2046
nizecode <nicolas1106sevilla@gmail.com>
2022-06-08 18:10:22 -0500

    bd y cosas varias

--- Exams/Exam-03/CreditBank.Api/Controllers/ReportedCardsController.cs
+++ Exams/Exam-03/CreditBank.Api/Controllers/ReportedCardsController.cs
@@ -1,93 +1,95 @@
 ﻿using System;
 using System.Net;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using CreditBank.Api.Models;
+using CreditBank.Api.Services;
+using CreditBank.Api.Exceptions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using CreditBank.Api.Models;
-using CreditBank.Api.Exceptions;
-using CreditBank.Api.Services;
-using System.Text.RegularExpressions;
+
 
 namespace CreditBank.Api.Controllers
 {
-    [Route("api/v1.0/[controller]")]
-    [ApiController]
-    public class ReportedCardsController : ControllerBase
-    {
-        private readonly ILogger<ReportedCardsController> _logger;
-        private readonly ReportedCardService _reportedCardService;
+   [Route("api/v1.0/[controller]")]
+   [ApiController]
+   public class ReportedCardsController : ControllerBase
+   {
+      private readonly ILogger<ReportedCardsController> _logger;
+      private readonly ReportedCardService _reportedCardService;
 
-        public ReportedCardsController(ReportedCardService reportedCardService, ILogger<ReportedCardsController> logger)
+      public ReportedCardsController(ReportedCardService reportedCardService, ILogger<ReportedCardsController> logger)
         {
             _logger = logger;
             _reportedCardService = reportedCardService;
         }
 
-        // GET: api/v1.0/<ReportedCardsController>
-        [HttpGet]
-        public async Task<ActionResult<IList<ReportedCard>>> GetAllReportedCards()
-        {
-            try
+      // GET: api/v1.0/<ReportedCardsController>
+      [HttpGet]
+      public async Task<ActionResult<IList<ReportedCard>>> GetAllReportedCards()
+      {
+         try
             {
-                return Ok(await _reportedCardService.GetAllReportedCards());
+                  return Ok(await _reportedCardService.GetAllReportedCards());
             }
-            catch (NotFoundException ex)
+            catch(NotFoundException ex)
             {
-                return NotFound(ex.Message);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
-            }
-        }
-
-        // GET: api/v1.0/ReportedCards/IssuingNetwork/{issuingNetworkName}
-        [HttpGet("IssuingNetwork/{issuingNetworkName}")]
-        public async Task<ActionResult<IList<ReportedCard>>> GetAllReportedCardsByIssuingNetworkName(string issuingNetworkName)
-        {
-            try
-            {
-                return Ok(await _reportedCardService.GetAllReportedCardsByIssuingNetworkName(issuingNetworkName));
-            }
-            catch (NotFoundException ex)
-            {
-                return NotFound(ex.Message);
+                  return NotFound(ex.Message);
             }
             catch (Exception ex)
             {
-                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+                  return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
             }
-        }
+      }
 
-        // GET: api/v1.0/<ReportedCard>/CrediCardNumber
-        [HttpGet("{creditCardNumber}")]
-        public async Task<ActionResult<ReportedCard>> GetReportedCard(string creditCardNumber)
-        {
-            return Ok(await _reportedCardService.GetReportedCard(creditCardNumber));
-        }
+      // GET: api/v1.0/ReportedCards/IssuingNetwork/{issuingNetworkName}
+      [HttpGet("IssuingNetwork/{issuingNetworkName}")]
+      public async Task<ActionResult<IList<ReportedCard>>> GetAllReportedCardsByIssuingNetworkName(string issuingNetworkName)
+      {
+         try
+         {
+               return Ok(await _reportedCardService.GetAllReportedCardsByIssuingNetworkName(issuingNetworkName));
+         }
+         catch (NotFoundException ex)
+         {
+               return NotFound(ex.Message);
+         }
+         catch (Exception ex)
+         {
+               return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+         }
+      }
 
-        // PUT: api/v1.0/ReportedCards/{creditCardNumber}
-        [HttpPut("{creditCardNumber}")]
-        public async Task<ActionResult<string>> PutCreditCardReactivated(string creditCardNumber)
-        {
-            try
-            {
-                return Ok(await _reportedCardService.PutCreditCardReactivated(creditCardNumber));
-            }
-            catch (NotFoundException ex)
-            {
-                return NotFound(ex.Message);
-            }
-            catch (BadRequestException ex)
-            {
-                return BadRequest(ex.Message);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
-            }
-        }
-    }
+      // GET: api/v1.0/<ReportedCard>/CrediCardNumber
+      [HttpGet("{creditCardNumber}")]
+      public async Task<ActionResult<ReportedCard>> GetReportedCard(string creditCardNumber)
+      {
+         return Ok(await _reportedCardService.GetReportedCard(creditCardNumber));
+      }
+
+      // PUT: api/v1.0/ReportedCards/{creditCardNumber}
+      [HttpPut("{creditCardNumber}")]
+      public async Task<ActionResult<string>> PutCreditCardReactivated(string creditCardNumber)
+      {
+         try
+         {
+               return Ok(await _reportedCardService.PutCreditCardReactivated(creditCardNumber));
+         }
+         catch (NotFoundException ex)
+         {
+               return NotFound(ex.Message);
+         }
+         catch (BadRequestException ex)
+         {
+               return BadRequest(ex.Message);
+         }
+         catch (Exception ex)
+         {
+               return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+         }
+      }
+   }
+
 }
--- Exams/Exam-03/CreditBank.Api/DataAccess/ReportedCardDataAccess.cs
+++ Exams/Exam-03/CreditBank.Api/DataAccess/ReportedCardDataAccess.cs
@@ -1,52 +1,43 @@
 ﻿using System;
 using System.Linq;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using CreditBank.Api.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
 namespace CreditBank.Api.DataAccess
 {
+    public class ReportedCardDataAccess
+    {
+        private readonly AppDbContext _dbContext;
 
-   public class ReportedCardDataAccess
-   {
+        public ReportedCardDataAccess(AppDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
 
-      private readonly AppDbContext _dbContext;
+        public async Task<IList<ReportedCard>> GetAllReportedCards()
+        {
+            return await _dbContext.ReportedCards.ToListAsync();
+        }
 
-      public ReportedCardDataAccess(AppDbContext dbContext){
+        public async Task<IList<ReportedCard>> GetAllReportedCardsByIssuingNetworkName(string issuingNetworkName)
+        {
+            return await _dbContext.ReportedCards.Where(item => item.IssuingNetwork == issuingNetworkName).ToListAsync();
+        }
 
-         _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        public async Task<ReportedCard> GetReportedCard(string creditCardNumber)
+        {
+            return await _dbContext.ReportedCards.Where(item => item.CreditCardNumber == creditCardNumber).FirstOrDefaultAsync();
+        }
 
-      }
+        public async Task<string> PutCreditCardReactivated(string creditCardNumber)
+        {
+            var reportedCard = await GetReportedCard(creditCardNumber);
+            reportedCard.StatusCard = "Recovered";
+            _dbContext.SaveChanges();
 
-      public async Task<IList<ReportedCard>> GetAllReportedCards(){
-
-         return await _dbContext.ReportedCards.ToListAsync();
-
-      }
-
-      public async Task<IList<ReportedCard>> GetAllReportedCardsByIssuingNetworkName(string issuingNetworkName){
-        
-         return await _dbContext.ReportedCards.Where(item => item.IssuingNetwork == issuingNetworkName).ToListAsync();
-      
-      }
-
-      public async Task<ReportedCard> GetReportedCard(string creditCardNumber){
-
-         return await _dbContext.ReportedCards.Where(item => item.CreditCardNumber == creditCardNumber).FirstOrDefaultAsync();
-      
-      }
-
-      public async Task<string> PutCreditCardReactivated(string creditCardNumber){
-
-         var reportedCard = await GetReportedCard(creditCardNumber);
-         reportedCard.StatusCard = "Recovered";
-         _dbContext.SaveChanges();
-
-         return "Credit card reccovered";
-      
-      }
-
-   }
-
+            return "Credit card recovered";
+        }
+    }
 }
