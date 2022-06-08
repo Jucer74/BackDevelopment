f2ed89fae069e2389432c7702649a463332142a6
nizecode <nicolas1106sevilla@gmail.com>
2022-06-08 01:03:44 -0500

    guardando antes de que suceda otra tragedia :) falta probar la base y corregir unas cositas

--- Exams/Exam-03/CreditBank.Api/DataAccess/ReportedCardDataAccess.cs
+++ Exams/Exam-03/CreditBank.Api/DataAccess/ReportedCardDataAccess.cs
@@ -1,6 +1,52 @@
-﻿namespace CreditBank.Api.DataAccess
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CreditBank.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CreditBank.Api.DataAccess
 {
+
    public class ReportedCardDataAccess
    {
+
+      private readonly AppDbContext _dbContext;
+
+      public ReportedCardDataAccess(AppDbContext dbContext){
+
+         _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+
+      }
+
+      public async Task<IList<ReportedCard>> GetAllReportedCards(){
+
+         return await _dbContext.ReportedCards.ToListAsync();
+
+      }
+
+      public async Task<IList<ReportedCard>> GetAllReportedCardsByIssuingNetworkName(string issuingNetworkName){
+        
+         return await _dbContext.ReportedCards.Where(item => item.IssuingNetwork == issuingNetworkName).ToListAsync();
+      
+      }
+
+      public async Task<ReportedCard> GetReportedCard(string creditCardNumber){
+
+         return await _dbContext.ReportedCards.Where(item => item.CreditCardNumber == creditCardNumber).FirstOrDefaultAsync();
+      
+      }
+
+      public async Task<string> PutCreditCardReactivated(string creditCardNumber){
+
+         var reportedCard = await GetReportedCard(creditCardNumber);
+         reportedCard.StatusCard = "Recovered";
+         _dbContext.SaveChanges();
+
+         return "Credit card reccovered";
+      
+      }
+
    }
+
 }
