ce4faa3364d444ffbb4c9743ce83359a364ede0d
oric99 <70826513+oric99@users.noreply.github.com>
2022-06-08 10:38:20 -0500

    parrrrrcial

---
+++ Exams/Exam-03/CreditBank.Api/DataAccess/AppDbContext.cs
@@ -0,0 +1,21 @@
+using CreditBank.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace CreditBank.Api.DataAccess
+{
+   public class AppDbContext : DbContext
+   {
+      public AppDbContext()
+      {
+      }
+
+      public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+      {
+      }
+
+      public DbSet<ReportedCard> ReportedCards { get; set; }
+   }
+}
---
+++ Exams/Exam-03/CreditBank.Api/Startup.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using CreditBank.Api.DataAccess;
+using CreditBank.Api.Models;
+using CreditBank.Api.Services;
+
+
+namespace CreditBank.Api
+{
+   public class Startup
+   {
+      public Startup(IConfiguration configuration)
+      {
+         Configuration = configuration;
+      }
+
+      public IConfiguration Configuration { get; }
+
+      // This method gets called by the runtime. Use this method to add services to the container.
+      public void ConfigureServices(IServiceCollection services)
+      {
+         services.AddControllers();
+         services.AddDbContext<AppDbContext>(options => options.UseSqlite("Name=CreditBankDB"));
+         services.AddScoped<ReportedCardDataAccess>();
+         services.AddScoped<ReportedCardService>();
+         services.AddSwaggerGen(c =>
+         {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "CreditBank.Api", Version = "v1" });
+         });
+      }
+
+      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+      public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+      {
+         if (env.IsDevelopment())
+         {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CreditBank.Api v1"));
+         }
+
+         app.UseHttpsRedirection();
+
+         app.UseRouting();
+
+         app.UseAuthorization();
+
+         app.UseEndpoints(endpoints =>
+         {
+            endpoints.MapControllers();
+         });
+      }
+   }
+}
