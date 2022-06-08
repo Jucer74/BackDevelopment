8dc02409b1d4f8271632104e20d5f83538a2ad9a
SEVFCA <sevfca246@gmail.com>
2022-06-08 17:14:27 -0500

    Api

---
+++ Exams/Exam-03/CreditBank.Api/DataAccess/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CreditBank.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CreditBank.Api.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ReportedCard> ReportedCards { get; set; }
+    }
+}
