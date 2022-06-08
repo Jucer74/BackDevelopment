66962c240e14bec7e51002ddba19e701e253b129
oric99 <70826513+oric99@users.noreply.github.com>
2022-06-08 14:41:31 -0500

    pruebas 1

--- Exams/Exam-03/CreditBank.Api/DataAccess/AppDbContext.cs
+++ Exams/Exam-03/CreditBank.Api/DataAccess/AppDbContext.cs
@@ -1,21 +1,19 @@
-using CreditBank.Api.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using CreditBank.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace CreditBank.Api.DataAccess
 {
-   public class AppDbContext : DbContext
-   {
-      public AppDbContext()
-      {
-      }
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
 
-      public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-      {
-      }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
 
-      public DbSet<ReportedCard> ReportedCards { get; set; }
-   }
+        public DbSet<ReportedCard> ReportedCards { get; set; }
+    }
 }
