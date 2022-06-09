aa832e85c2bf5a8261a7b2d61aa059e409b3e60c
jhon03 <hoyosb90@gmail.com>
2022-06-08 20:09:04 -0500

    Final del Parcial_por fin: lo que fue fue

--- Exams/Exam-03/CreditBank.Api/Controllers/ReportedCardsController.cs
+++ Exams/Exam-03/CreditBank.Api/Controllers/ReportedCardsController.cs
@@ -1,57 +1,126 @@
-﻿using System; 
+﻿using System;
+using System.Net;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using CreditBank.Api.Models;
-using System.Net;
+using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
-using System.Collections.Generic;
+using CreditBank.Api.Models;
 using CreditBank.Api.Services;
- using Microsoft.Extensions.Logging;
-
-
-using CreditBank.Api.Utilities;
 using CreditBank.Api.Exceptions;
+using CreditBank.Api.Utilities;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace CreditBank.Api.Controllers
 {
-   [Route("api/v1.0/[controller]")]
-   [ApiController]
-   public class ReportedCardsController : ControllerBase
-   {
+    [Route("api/v1.0/[controller]")]
+    [ApiController]
+    public class ReportedCardsController : ControllerBase
+    {
+        private readonly ILogger<ReportedCardsController> _logger;
+        private readonly ReportedCardService _reportedCardService;
 
-      private readonly ReportedCardService _reportedCardService;
-
-      private readonly ILogger<ReportedCardsController> _logger;
-      
-      public ReportedCardsController(ReportedCardService reportedCardService, ILogger<ReportedCardsController> logger)
+        public ReportedCardsController(ReportedCardService reportedCardService, ILogger<ReportedCardsController> logger)
         {
             _logger = logger;
             _reportedCardService = reportedCardService;
         }
 
-      [HttpGet]
-      public async Task<ActionResult<IList<ReportedCard>>> GetAllReportedCards()
-      {
-         return Ok(await _reportedCardService.GetAllReportedCards());
-      }
+        [HttpGet]
+        public async Task<ActionResult<IList<ReportedCard>>> GetAllReportedCards()
+        {
+            try
+            {
+                return Ok(await _reportedCardService.GetAllReportedCards());
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
 
-      // GET: api/v1.0/<ReportedCardsController>/IssuingNetwork/{issuingNetworkName}
-      [HttpGet("IssuingNetwork/{issuingNetworkName}")]
-      public async Task<ActionResult<IEnumerable<ReportedCard>>> GetAllReportedCardsByIssuingNetworkName(string issuingNetworkName)
-      {
-         try
-         {
-            return Ok(await _reportedCardService.GetAllReportedCardsByIssuingNetworkName(issuingNetworkName));
-         }
-         catch (NotFoundException ex)
-         {
-            return NotFound(ex.Message);
-         }
-         catch (Exception ex)
-         {
-            return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
-         }
-      }
-}
+        [HttpGet("IssuingNetwork/{issuingNetworkName}")]
+        public async Task<ActionResult<IList<ReportedCard>>> GetAllReportedCardsByIssuingNetworkName(string issuingNetworkName)
+        {
+            try
+            {
+                return Ok(await _reportedCardService.GetAllReportedCardsByIssuingNetworkName(issuingNetworkName));
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
 
+        [HttpGet("{creditCardNumber}")]
+        public async Task<ActionResult<ReportedCard>> GetReportedCard(string creditCardNumber)
+        {
+             try
+            {
+                if (!CreditCardValidator.IsNumericCard(creditCardNumber))
+                {
+                    return BadRequest($"{creditCardNumber} is NOT Numeric");
+                }
+
+                if (CreditCardValidator.IsValid(creditCardNumber))
+                {
+                    return Ok(await _reportedCardService.GetReportedCard(creditCardNumber));
+                }
+
+                return Ok($"{creditCardNumber} is NOT Valid");
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (BadRequestException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPut("{creditCardNumber}")]
+        public async Task<ActionResult<ReportedCard>> PutCreditCardReactivated(string creditCardNumber)
+        {
+             try
+            {
+            if (!CreditCardValidator.IsNumericCard(creditCardNumber))
+                {
+                    return BadRequest($"{creditCardNumber} is NOT Numeric");
+                }
+
+            if (CreditCardValidator.IsValid(creditCardNumber))
+                {
+                    return Ok(await _reportedCardService.PutCreditCardReactivated(creditCardNumber));
+                }
+
+            return Ok($"{creditCardNumber} is NOT Valid");
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (BadRequestException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+            
+        }
+    }
 }
