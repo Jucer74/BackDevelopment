b16ed6ba4e13638479156732cbcb3eeb2f20db65
joseda1807 <pedrodo221@hotmail.com>
2022-09-07 19:13:08 -0500

    Update Program.cs

--- Practices/01.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
+++ Practices/01.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
@@ -5,6 +5,7 @@
  * For numbers which are multiples of both three and five print “M-3-5”
  * */
 using System;
+using Internal;
 
 namespace PrintMultiples
 {
@@ -12,7 +13,29 @@ namespace PrintMultiples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            for (int i = 0; i <= 100; i = 1++)
+            {
+                if (i % 3 == 0 && i % 5 == 0){
+                    Console.Write("M-3-5");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.Write("M-3");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write("M-5");
+                }
+                else
+                {
+                    Console.Write(i);
+                }
+                    else if (i % 3 == 0)
+                {
+                    Console.Write("M-3");
+                }
+
+
+            }
         }
-    }
-}
