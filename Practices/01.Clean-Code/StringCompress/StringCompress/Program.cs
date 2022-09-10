3f52bfbf1e3a8b6a618555b03e362f978a49444b
Julio Robles <Jucer74@hotmail.com>
2022-09-10 15:36:10 -0500

    Fix Errors

--- Practices/01.Clean-Code/StringCompress/StringCompress/Program.cs
+++ Practices/01.Clean-Code/StringCompress/StringCompress/Program.cs
@@ -4,25 +4,28 @@
 * a2b1c5a3. If the "compressed" string would not become smaller than the
 * original string, your method should return the original string.
 */
-
 using System;
 
-public class Program
+namespace StringCompress
 {
-	public static void Main()
+	public static class Program
 	{
-		Compress("aabcccccaaa");
-		// aabcccccaaa => a2b1c5a3
-		Compress("abbcca");
-		// abbcca => abbcca
-		Compress("aabbcc");
-		// aabbcc => aabbcc
-		Compress("");
-		// Error: Empty String
-	}
+		public static void Main()
+		{
+			Compress("aabcccccaaa");
+			// aabcccccaaa => a2b1c5a3
+			Compress("abbcca");
+			// abbcca => abbcca
+			Compress("aabbcc");
+			// aabbcc => aabbcc
+			Compress("");
+			// Error: Empty String
+		}
 
 
-	private static void Compress(string input)
-	{
+		private static void Compress(string input)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
