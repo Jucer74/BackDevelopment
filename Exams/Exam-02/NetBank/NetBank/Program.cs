0f2cf44feab329089e2182595552364f4ed8e7ad
Julio Robles <Jucer74@hotmail.com>
2022-10-13 03:54:45 -0500

    Adding Validation

--- Exams/Exam-02/NetBank/NetBank/Program.cs
+++ Exams/Exam-02/NetBank/NetBank/Program.cs
@@ -4,14 +4,15 @@
 
 ApplicationData applicationData = new();
 
-#endregion
+#endregion Variables
 
 Console.WriteLine("NetBank Application");
 
 //var reportedCardsList = applicationData.GetReportedCards();
 //Console.WriteLine(reportedCardsList.Count);
 
-var result = applicationData.UpdateReportedCard("6376773337234061");
-Console.WriteLine(result);
+//var result = applicationData.UpdateReportedCard("6376773337234061");
+//Console.WriteLine(result);
 
-   
+//var isValid = CreditCardBL.IsValid("6376773337234061");
+//Console.WriteLine(isValid);
