63909b2abfc7f166a3f8dda9dae44bd5187202c8
SnowBaxter <diegoandres.u26@gmail.com>
2022-10-19 20:41:18 -0500

    sisi

--- Exams/Exam-02/NetBank/NetBank/DataAccess/Define.cs
+++ Exams/Exam-02/NetBank/NetBank/DataAccess/Define.cs
@@ -2,7 +2,7 @@
 {
    public static class Constants
    {
-      public static readonly string DatabaseFileName = @"..\..\..\..\..\Database\NetBankDB.db";
+      public static readonly string DatabaseFileName = @"../../../../../Database/NetBankDB.db";
       public static readonly string SelectReportedCards = "SELECT Id, IssuingNetwork, CreditCardNumber, FirstName, LastName, StatusCard, ReportedDate, LastUpdatedDate FROM ReportedCards";
       public static readonly string SelectReportedCardByCardNumber = "SELECT Id, IssuingNetwork, CreditCardNumber, FirstName, LastName, StatusCard, ReportedDate, LastUpdatedDate FROM ReportedCards WHERE CreditCardNumber = $CreditCardNumber";
       public static readonly string UpdateReportedCards = "UPDATE ReportedCards SET StatusCard = $StatusCard  , LastUpdatedDate = datetime('now') WHERE CreditCardNumber = $CreditCardNumber";
