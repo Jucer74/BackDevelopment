56793e1c48dc3068eedb953becaf20c15b32c279
Julio Robles <Jucer74@Hotmail.com>
2022-10-24 22:35:25 -0500

    Update Base

--- Exams/Exam-02/NetBank/NetBank/Program.cs
+++ Exams/Exam-02/NetBank/NetBank/Program.cs
@@ -8,7 +8,9 @@ ApplicationData applicationData = new();
 
 Console.WriteLine("NetBank Application");
 
-//var reportedCardsList = applicationData.GetReportedCards();
+var reportedCardsList = applicationData.GetReportedCards();
+PrintReportedCards(reportedCardsList); 
+
 //Console.WriteLine(reportedCardsList.Count);
 
 //var result = applicationData.UpdateReportedCard("6376773337234061");
@@ -16,3 +18,22 @@ Console.WriteLine("NetBank Application");
 
 //var isValid = CreditCardBL.IsValid("6376773337234061");
 //Console.WriteLine(isValid);
+
+void PrintReportedCards(List<ReportedCard> reportedCards)
+{
+   // Title
+   Console.WriteLine("Id,IssuingNetwork,CreditCardNumber,FirstName,LastName,StatusCard,ReportedDate,LastUpdatedDate");
+
+   // Loop to print the records
+   foreach (var reportedCard in reportedCards)
+   {
+      Console.WriteLine("{0},{1},{2},{3},{4},{5},{6:MM/dd/yyyy},{7:MM/dd/yyyy}", reportedCard.Id,
+                                                                                 reportedCard.IssuingNetwork,
+                                                                                 reportedCard.CreditCardNumber,
+                                                                                 reportedCard.FirstName,
+                                                                                 reportedCard.LastName,
+                                                                                 reportedCard.StatusCard,
+                                                                                 reportedCard.ReportedDate,
+                                                                                 reportedCard.LastUpdatedDate);
+   }
+}
