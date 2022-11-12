8f3dd707d2e6e463d8a2f655f8d17e411272b2d6
Julio Robles <Jucer74@hotmail.com>
2022-11-12 14:35:36 -0500

    Update Repo

---
+++ Proyecto/ProductsApi/Context/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsApi.Models;
+
+namespace ProductsApi.Context;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("Name=ProductsDB");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.Property(e => e.Description).HasColumnType("VARCHAR (255)");
+            entity.Property(e => e.ImageName).HasColumnType("VARCHAR (255)");
+            entity.Property(e => e.Name).HasColumnType("VARCHAR (50)");
+            entity.Property(e => e.Price).HasColumnType("DECIMAL (10, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
