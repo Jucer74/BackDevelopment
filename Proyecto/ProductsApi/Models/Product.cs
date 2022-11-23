8341f46d6ce1d5bf0acdf055716aa611b143d780
SnowBaxter <diegoandres.u26@gmail.com>
2022-11-23 15:10:47 -0500

    11

--- Proyecto/ProductsApi/Models/Product.cs
+++ Proyecto/ProductsApi/Models/Product.cs
@@ -2,23 +2,19 @@
 
 namespace ProductsApi.Models
 {
-    public class Product
-    {
-        [Key]
-        public int Id { get; set; }
+   public class Product
+   {
+      [Key]
+      public int Id { get; set; }
 
-        [Required(ErrorMessage = "El Nombre es requerido")]
-        [StringLength(50, ErrorMessage = "La longitud maxima del Nombre es de 50 caracteres")]
-        public string Name { get; set; } = null!;
+      [Required(ErrorMessage = "El nombre es requerido")]
+      public string? Name { get; set; }
 
-        [StringLength(255, ErrorMessage = "La longitud maxima de la Descripcion es de 255 caracteres")]
-        public string? Description { get; set; }
+      public string? Description { get; set; }
 
-        [Required(ErrorMessage = "El Precio es requerido")]
-        public decimal Price { get; set; }
+      [Required(ErrorMessage ="El Precio es requerido")]
+      public decimal? Price { get; set; }
 
-        [Required(ErrorMessage = "El Nombre de la Imagen es requerido")]
-        [StringLength(50, ErrorMessage = "La longitud maxima del Nombre de la Imagen es de 50 caracteres")]
-        public string? ImageName { get; set; }
-    }
+      public string? ImageName { get; set; }
+   }
 }
