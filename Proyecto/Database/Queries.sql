2028dc45ebebfc3f5db7886bed32560d0f2c45f8
SnowBaxter <diegoandres.u26@gmail.com>
2022-11-23 15:05:16 -0500

    changes

---
+++ Proyecto/Database/Queries.sql
@@ -0,0 +1,7 @@
+-- Truncate Table
+/*
+DELETE FROM Products;
+UPDATE `sqlite_sequence` SET `seq` = 0 WHERE `name` = 'Products';
+*/
+
+SELECT COUNT(1) FROM Products
--- Proyecto/ProductsApi/Models/Product.cs
+++ Proyecto/ProductsApi/Models/Product.cs
@@ -2,19 +2,23 @@
 
 namespace ProductsApi.Models
 {
-   public class Product
-   {
-      [Key]
-      public int Id { get; set; }
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
 
-      [Required(ErrorMessage = "El nombre es requerido")]
-      public string? Name { get; set; }
+        [Required(ErrorMessage = "El Nombre es requerido")]
+        [StringLength(50, ErrorMessage = "La longitud maxima del Nombre es de 50 caracteres")]
+        public string Name { get; set; } = null!;
 
-      public string? Description { get; set; }
+        [StringLength(255, ErrorMessage = "La longitud maxima de la Descripcion es de 255 caracteres")]
+        public string? Description { get; set; }
 
-      [Required(ErrorMessage ="El Precio es requerido")]
-      public decimal? Price { get; set; }
+        [Required(ErrorMessage = "El Precio es requerido")]
+        public decimal Price { get; set; }
 
-      public string? ImageName { get; set; }
-   }
+        [Required(ErrorMessage = "El Nombre de la Imagen es requerido")]
+        [StringLength(50, ErrorMessage = "La longitud maxima del Nombre de la Imagen es de 50 caracteres")]
+        public string? ImageName { get; set; }
+    }
 }
