d037acf94b82774bbfce8a62741006dbc50a49da
Julio Robles <Jucer74@Hotmail.com>
2023-03-03 17:55:15 -0500

    Practica OOP

---
+++ Practices/02.OOP/PracticeOOP/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PracticeOOP;
+
+Console.WriteLine("Hello, World!");
+
+
+Triangle triangle = new Triangle(3, 5);
+var hipotenuse = triangle.Hipotenuse;
+Console.WriteLine(hipotenuse.ToString());
---
+++ Practices/02.OOP/PracticeOOP/Triangle.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading.Tasks.Sources;
+
+namespace PracticeOOP;
+
+/// <summary>
+/// Triangle Rectangle
+/// </summary>
+public class Triangle
+{
+    /// <summary>
+    /// Side1 in the rectangle angle
+    /// </summary>
+    public double Side1 { get; set; } = 0;
+
+    /// <summary>
+    /// Side2 in the Rectangle angle
+    /// </summary>
+    public double Side2 { get; set; } = 0;
+
+    /// <summary>
+    /// Calculate the Hipotenuse using the square root of  the addition 
+    /// of side1 elevate to squere and the side2 elevate to square
+    /// </summary>
+    public double Hipotenuse
+    {
+        get
+        {
+            return Math.Sqrt(Math.Pow(Side1, 2) + Math.Pow(Side2, 2));
+        }
+    }
+
+    /// <summary>
+    /// DEfault Constructor
+    /// </summary>
+    public Triangle()
+    {
+        Side1 = 0;
+        Side2 = 0;
+    }
+
+    /// <summary>
+    /// Constructor with Parameters
+    /// </summary>
+    /// <param name="side1">Side 1 in the rectangle Angle</param>
+    /// <param name="side2">Side 2 in the rectangle Angle</param>
+    public Triangle(double side1, double side2)
+    {
+        Side1 = side1;
+        Side2 = side2;
+    }
+
+    /// <summary>
+    /// Calculate the Perimetr using the values for Side1, Side2 and the Hipotenuse
+    /// </summary>
+    /// <returns></returns>
+    public double GetPerimeter()
+    {
+        return Side1 + Side2 + Hipotenuse;
+    }
+}
