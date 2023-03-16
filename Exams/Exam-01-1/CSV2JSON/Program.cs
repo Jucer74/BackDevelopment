18bfefe5dec7160c5915666f7256b8927fb7a645
AFSilver <felipe.correa@beltcom.co>
2023-03-15 21:03:23 -0500

    Merge branch 'main' of https://github.com/Jucer74/BackDevelopment

--- Exams/Exam-01-1/CSV2JSON/Program.cs
+++ Exams/Exam-01-1/CSV2JSON/Program.cs
@@ -1,43 +1,68 @@
 ﻿using System.IO;
 using Microsoft.VisualBasic.FileIO;
 using Newtonsoft.Json;
+//-----------------------------------------------------------------------------------------------------------------
+const string DEFAULT_DELIMITER = ",";
 
 Console.WriteLine("CSV2JSON - Convert CSV file to JSON");
 Console.WriteLine("-----------------------------------");
-
 Main();
 
 void Main()
 {
-    // Ruta del archivo CSV
+    BuildJSON(  ConvertToJSON( ReadCSV( GetCSVRoute() ) ), SetJSONRoute()  );
+}
+
+string GetCSVRoute(){ // Ruta del archivo CSV
     string inputFileCsv = "./Data/DATA.csv";
+    return inputFileCsv;
+}
+
+List<string[]?> ReadCSV(string inputFileCsv){ //Leer CSV
+    try{
+        using ( TextFieldParser CsvParser = new TextFieldParser(inputFileCsv) )
+        {
+            SetCSVDelimiter(CsvParser, DEFAULT_DELIMITER);
+            // Leer los datos del archivo CSV
+            string[]? headers = CsvParser.ReadFields();
+            List<string[]?> rows = LoadCsvData(CsvParser);
+            return rows;
+        }
+    }catch(System.IO.IOException nonFoundCsv){ //Exception thrown if file not found
+        Console.WriteLine("Exception caught: {0}", nonFoundCsv);
+    }
+    return new List<string[]?>(); //return empty list if exception catch, it will create an empty JSON file so delete it
+}
+
+void SetCSVDelimiter(TextFieldParser CsvParser, string DEFAULT_DELIMITER){
+    CsvParser.TextFieldType = FieldType.Delimited;
+    CsvParser.SetDelimiters(DEFAULT_DELIMITER);
+}
+
+List<string[]?> LoadCsvData(TextFieldParser CsvParser){
+    List<string[]?> rows = new List<string[]?>();
+    while (!CsvParser.EndOfData)
+        {
+            rows.Add( CsvParser.ReadFields() );
+        }     
+    return rows;
+}
+
+string ConvertToJSON(List<string[]?> rows){
+    var jsonObject = new { data = rows };
+    string json = JsonConvert.SerializeObject(jsonObject);
+    return json;
+}
+
+string SetJSONRoute(){ // Ruta donde terminará archivo JSON
     string outputFileJson = "./Data/DATA.json";
+    return outputFileJson;
+}
 
-    // Leer el archivo CSV
-    using (TextFieldParser parser = new TextFieldParser(inputFileCsv))
+void BuildJSON(string json, string outputFileJson){
+    Console.WriteLine(json);
+    using (TextWriter JsonWriter = new StreamWriter(outputFileJson))
     {
-        parser.TextFieldType = FieldType.Delimited;
-        parser.SetDelimiters(",");
-        
-        // Leer los datos del archivo CSV
-        string[] headers = parser.ReadFields();
-        string[][] rows = new string[0][];
-        while (!parser.EndOfData)
-        {
-            string[] row = parser.ReadFields();
-            Array.Resize(ref rows, rows.Length + 1);
-            rows[rows.Length - 1] = row;
-        }
-
-        // Convertir los datos a JSON
-        var jsonObject = new { data = rows };
-        string json = JsonConvert.SerializeObject(jsonObject);
-        
-        // Escribir el resultado
-        Console.WriteLine(json);
-        using (TextWriter writer = new StreamWriter(outputFileJson))
-        {
-            writer.WriteLine(json);
-        }
+        JsonWriter.WriteLine(json);
     }
 }
