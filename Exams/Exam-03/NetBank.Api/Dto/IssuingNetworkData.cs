462643afbfa6a56f8ecbeee3131e508a97c8793c
Julio Robles <Jucer74@Hotmail.com>
2023-05-24 19:24:44 -0500

    Update IssuingNetworkData.cs

--- Exams/Exam-03/NetBank.Api/Dto/IssuingNetworkData.cs
+++ Exams/Exam-03/NetBank.Api/Dto/IssuingNetworkData.cs
@@ -2,7 +2,7 @@
 
 public class IssuingNetworkData
 {
-    public string IssuingNetworkName { get; set; } = null!;
+    public string Name { get; set; } = null!;
     public List<int>? StartsWithNumbers { get; set; } = null!;
     public RangeNumber? InRange { get; set; } = null!;
     public List<int> AllowedLengths { get; set; } = null!;
