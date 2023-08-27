7e83c9a660c10c51a764528b7c979ef48a41144c
Cesar Eduardo Mejia Buitrago <cmejia@greensqa.com>
2023-08-26 21:31:57 -0500

    Implement menu

--- Exams/Exam-01/BankApp/Program.cs
+++ Exams/Exam-01/BankApp/Program.cs
@@ -1,6 +1,39 @@
 ﻿using System;
+using System.Linq.Expressions;
 
-class Program
+try
+    {
+        Menu();
+    }catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+
+    void Menu()
+    {
+        var option = 0;
+        while((Options)option != Options.Exit)
+        {
+        Console.Write("1. Create Account\n" + "2. Get Balance\n" + "3. Deposit Amount\n" + "4. Withdrawal Amount\n" + "0. Exit\n" + "Select Option:\n");
+        option = Console.Read();
+
+        if((Options)option == Options.CreateAccount)
+        {}
+        else if((Options)option == Options.GetBalance)
+        {}
+        else if((Options)option == Options.DepositAmount)
+        {}
+        else if((Options)option == Options.WithdrawalAmount)
+        {}
+        }
+
+    }
+
+public enum Options
 {
-
+    CreateAccount = 1,
+    GetBalance = 2,
+    DepositAmount = 3,
+    WithdrawalAmount = 4,
+    Exit = 0
 }
