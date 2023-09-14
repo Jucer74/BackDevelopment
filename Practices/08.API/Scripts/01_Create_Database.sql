f82a953ab75e637f0cc1f69d9457ff23cc8226ff
Julio Robles <Jucer74@Hotmail.com>
2023-09-13 20:09:22 -0500

    Scripts

--- Practices/08.API/Scripts/01_Create_Database.sql
+++ Practices/08.API/Scripts/01_Create_Database.sql
@@ -1,1 +1,1 @@
-CREATE DATABASE `employeesdb`;
+CREATE DATABASE `studentsdb`;
--- Practices/08.API/Scripts/02_Create_User.sql
+++ Practices/08.API/Scripts/02_Create_User.sql
@@ -1,4 +1,4 @@
-/*DROP USER 'employeeuser'@'localhost' ;*/
-CREATE USER 'employeeuser'@'localhost' IDENTIFIED BY 'Empl0y33Us3r*01';
-GRANT ALL PRIVILEGES ON *.* TO 'employeeuser'@'localhost' WITH GRANT OPTION;
+/*DROP USER 'studentsuser'@'localhost' ;*/
+CREATE USER 'studentsuser'@'localhost' IDENTIFIED BY 'Stud3ntsUs3r*01';
+GRANT ALL PRIVILEGES ON *.* TO 'studentsuser'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
