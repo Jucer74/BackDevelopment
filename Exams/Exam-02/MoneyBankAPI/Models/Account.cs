399ea54ab37e1e256f15af334244e1bc9a6f82b0
Julio Robles <Jucer74@Hotmail.com>
2023-10-04 21:07:26 -0500

    Update

--- Exams/Exam-02/MoneyBankAPI/Models/Account.cs
+++ Exams/Exam-02/MoneyBankAPI/Models/Account.cs
@@ -2,5 +2,12 @@
 {
     public class Account
     {
+        public int Id { get; set; }
+        public char AccountType { get; set; } = 'A';
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        public string AccountNumber { get; set; } = null!;
+        public string OwnerName { get; set; } = null!;
+        public decimal BalanceAmount { get; set; }
+        public decimal OverdraftAmount { get; set; }
     }
 }
