7d0643a72ce6c45a5441d8461f0a0816d645bf87
Julio Robles <Jucer74@Hotmail.com>
2023-10-25 23:14:07 -0500

    Create QA_Clean.sql

---
+++ Exams/Exam-02/Scripts/QA_Clean.sql
@@ -0,0 +1,12 @@
+/* Delete Previous Data and Reset the Auto_Incremental Keys */
+TRUNCATE TABLE `moneybankdb`.`accounts` ;
+
+/************/
+/* Accounts */
+/************/
+INSERT INTO `moneybankdb`.`accounts` VALUES
+(1, 'C', '2023-06-30', '3016892501', 'Yurley Orejuela Ramirez', 1500000, 0 ), 
+(2, 'A', '2023-07-23', '5263418932', 'Libia Oquenda', 250000, 0 ), 
+(3, 'C', '2023-08-25', '6703001327', 'John Fredy Vásquez Izquierdo', 50000, 950000 );
+
+  
