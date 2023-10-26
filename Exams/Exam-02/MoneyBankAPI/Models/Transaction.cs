50fa25c7d6a7b64003afa83901473468e4feab75
Julio Robles <Jucer74@Hotmail.com>
2023-10-26 06:38:03 -0500

    Update And Fix

--- Exams/Exam-02/MoneyBankAPI/Models/Transaction.cs
+++ Exams/Exam-02/MoneyBankAPI/Models/Transaction.cs
@@ -12,9 +12,9 @@ namespace MoneyBankAPI.Models
         [Required(ErrorMessage = "Número de cuenta requerido")]
         [MaxLength(10, ErrorMessage = "El  campo de número de cuenta tiene una longitud máxima de 10 caracteres")]
         [RegularExpression(@"\d{10}", ErrorMessage = "El campo de numero de cuenta solo admite números")]
-        public string AccountNo { get; set; } = null!;
+        public string AccountNumber { get; set; } = null!; // Corrijo para Poder Probrar
 
         [RegularExpression(@"^\d+.?\d{0,2}$", ErrorMessage = "El valor debe estar en formato de moneda (0.00)")]
-        public decimal Amount { get; set; }
+        public decimal ValueAmount { get; set; } // Corrijo el Nombre para poder Probar
     }
 }
